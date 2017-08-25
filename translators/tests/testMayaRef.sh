6ecfe20b3b1cb8e739691f4c7769261de6046ca5
Fabrice Macagno <fabrice.macagno@al.com.au>
2017-08-25 17:46:49 +1000

    Reorganise tests

--- translators/tests/testMayaRef.sh
+++ translators/tests/testMayaRef.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export MAYA_PLUG_IN_PATH=$1/plugin:${MAYA_PLUG_IN_PATH}
+export MAYA_PLUG_IN_PATH=$1/plugin:$MAYA_PLUG_IN_PATH
 export PXR_PLUGINPATH=$1/share/usd/plugins:$PXR_PLUGINPATH
 export PYTHONPATH=$1/lib/python:$PYTHONPATH
+export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
 
 mayapy testMayaRef.py
