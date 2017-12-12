03e7063dd21c2a5ae303f40fa9dffddcaa7abc9d
Paul Molodowitch <pm@stanfordalumni.org>
2017-12-12 12:28:03 -0800

    add test for xformable maya reference

--- translators/tests/testMayaRef.py
+++ translators/tests/testMayaRef.py
@@ -19,7 +19,14 @@ class testTranslatorsPlugin(unittest.TestCase):
 	def testPluginIsFunctional(self):
 		mc.AL_usdmaya_ProxyShapeImport(file='./testMayaRef.usda')
 		self.assertTrue(Tf.Type.Unknown != Tf.Type.FindByName('AL::usdmaya::fileio::translators::MayaReference'))
-		self.assertTrue(len(mc.ls('cube:pCube1')))
+		self.assertEqual(1, len(mc.ls('cube:pCube1')))
+		self.assertEqual('mayaRefPrim', mc.listRelatives('cube:pCube1', parent=1)[0])
+		self.assertEqual((0.0, 0.0, 0.0), cmds.getAttr('mayaRefPrim.translate')[0])
+		self.assertFalse(mc.getAttr('mayaRefPrim.translate', lock=1))
+		self.assertEqual(1, len(mc.ls('otherNS:pCube1')))
+		self.assertEqual('otherCube', mc.listRelatives('otherNS:pCube1', parent=1)[0])
+		self.assertEqual((3.0, 2.0, 1.0), cmds.getAttr('otherCube.translate')[0])
+		self.assertFalse(mc.getAttr('otherCube.translate', lock=1))
 
 if __name__ == '__main__':
     unittest.main()
