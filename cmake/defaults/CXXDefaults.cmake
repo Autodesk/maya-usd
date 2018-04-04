e20a82ff63ee1c1dd5e291231a99ae6851905a70
Fabrice Macagno <Fabrice.Macagno@al.com.au>
2018-04-04 18:16:25 +1000

    Updated cmake files

--- cmake/defaults/CXXDefaults.cmake
+++ cmake/defaults/CXXDefaults.cmake
@@ -23,6 +23,7 @@
 #
 include(CXXHelpers)
 include(Version)
+include(Options)
 
 if (CMAKE_COMPILER_IS_GNUCXX)
     include(gccdefaults)
@@ -31,8 +32,6 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 elseif(MSVC)
     include(msvcdefaults)
 endif()
-
-_add_define(BUILD_COMPONENT_SRC_PREFIX="pxr/")
 
 _add_define(GL_GLEXT_PROTOTYPES)
 _add_define(GLX_GLXEXT_PROTOTYPES)
@@ -45,9 +44,7 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     _add_define(LINUX)
 endif()
 
-if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
-    _add_define(BUILD_OPTLEVEL_OPT)
-elseif(CMAKE_BUILD_TYPE STREQUAL "Debug")
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     _add_define(BUILD_OPTLEVEL_DEV)
 endif()
 
@@ -56,3 +53,42 @@ set(_PXR_CXX_FLAGS ${_PXR_CXX_FLAGS} ${_PXR_CXX_WARNING_FLAGS})
 # CMake list to string.
 string(REPLACE ";" " "  _PXR_CXX_FLAGS "${_PXR_CXX_FLAGS}")
 
+# Set namespace configuration.
+if (PXR_ENABLE_NAMESPACES)
+    set(PXR_USE_NAMESPACES "1")
+
+    if (PXR_SET_EXTERNAL_NAMESPACE)
+        set(PXR_EXTERNAL_NAMESPACE ${PXR_SET_EXTERNAL_NAMESPACE})
+    else()
+        set(PXR_EXTERNAL_NAMESPACE "pxr")
+    endif()
+
+    if (PXR_SET_INTERNAL_NAMESPACE)
+        set(PXR_INTERNAL_NAMESPACE ${PXR_SET_INTERNAL_NAMESPACE})
+    else()
+        set(PXR_INTERNAL_NAMESPACE "pxrInternal_v${PXR_MAJOR_VERSION}_${PXR_MINOR_VERSION}")
+    endif()
+
+    message(STATUS "C++ namespace configured to (external) ${PXR_EXTERNAL_NAMESPACE}, (internal) ${PXR_INTERNAL_NAMESPACE}")
+else()
+    set(PXR_USE_NAMESPACES "0")
+    message(STATUS "C++ namespaces disabled.")
+endif()
+
+# Set Python configuration
+if (PXR_ENABLE_PYTHON_SUPPORT)
+    set(PXR_PYTHON_SUPPORT_ENABLED "1")
+else()
+    set(PXR_PYTHON_SUPPORT_ENABLED "0")
+endif()
+
+# XXX: This is a workaround for an issue in which Python headers unequivocally
+# redefine macros defined in standard library headers. This behavior 
+# prevents users from running strict builds with PXR_STRICT_BUILD_MODE
+# as the redefinition warnings would cause build failures.
+#
+# The python official docs call this out here:
+# https://docs.python.org/2/c-api/intro.html#include-files
+#
+# The long term plan is to adhere to the required behavior.
+include_directories(SYSTEM ${PYTHON_INCLUDE_DIR})
---
+++ cmake/defaults/Options.cmake
@@ -0,0 +1,105 @@
+#
+# Copyright 2016 Pixar
+#
+# Licensed under the Apache License, Version 2.0 (the "Apache License")
+# with the following modification; you may not use this file except in
+# compliance with the Apache License and the following modification to it:
+# Section 6. Trademarks. is deleted and replaced with:
+#
+# 6. Trademarks. This License does not grant permission to use the trade
+#    names, trademarks, service marks, or product names of the Licensor
+#    and its affiliates, except as required to comply with Section 4(c) of
+#    the License and to reproduce the content of the NOTICE file.
+#
+# You may obtain a copy of the Apache License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Apache License with the above modification is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the Apache License for the specific
+# language governing permissions and limitations under the Apache License.
+#
+option(PXR_STRICT_BUILD_MODE "Turn on additional warnings. Enforce all warnings as errors." OFF)
+option(PXR_VALIDATE_GENERATED_CODE "Validate script generated code" OFF)
+option(PXR_HEADLESS_TEST_MODE "Disallow GUI based tests, useful for running under headless CI systems." OFF)
+option(PXR_BUILD_TESTS "Build tests" ON)
+option(PXR_BUILD_IMAGING "Build imaging components" ON)
+option(PXR_BUILD_EMBREE_PLUGIN "Build embree imaging plugin" OFF)
+option(PXR_BUILD_USD_IMAGING "Build USD imaging components" ON)
+option(PXR_BUILD_KATANA_PLUGIN "Build usd katana plugin" OFF)
+option(PXR_BUILD_MAYA_PLUGIN "Build usd maya plugin" OFF)
+option(PXR_BUILD_ALEMBIC_PLUGIN "Build the Alembic plugin for USD" OFF)
+option(PXR_BUILD_HOUDINI_PLUGIN "Build the Houdini plugin for USD" OFF)
+option(PXR_BUILD_DOCUMENTATION "Generate doxygen documentation" OFF)
+option(PXR_ENABLE_PYTHON_SUPPORT "Enable Python based components for USD" ON)
+option(PXR_ENABLE_MULTIVERSE_SUPPORT "Enable Multiverse backend in the Alembic plugin for USD" OFF)
+option(PXR_ENABLE_HDF5_SUPPORT "Enable HDF5 backend in the Alembic plugin for USD" ON)
+option(PXR_ENABLE_PTEX_SUPPORT "Enable Ptex support" ON)
+option(PXR_MAYA_TBB_BUG_WORKAROUND "Turn on linker flag (-Wl,-Bsymbolic) to work around a Maya TBB bug" OFF)
+option(PXR_ENABLE_NAMESPACES "Enable C++ namespaces." ON)
+
+# Precompiled headers are a win on Windows, not on gcc.
+set(pxr_enable_pch "OFF")
+if(MSVC)
+    set(pxr_enable_pch "ON")
+endif()
+option(PXR_ENABLE_PRECOMPILED_HEADERS "Enable precompiled headers." "${pxr_enable_pch}")
+set(PXR_PRECOMPILED_HEADER_NAME "pch.h"
+    CACHE
+    STRING
+    "Default name of precompiled header files"
+)
+
+set(PXR_INSTALL_LOCATION ""
+    CACHE
+    STRING
+    "Intended final location for plugin resource files."
+)
+
+set(PXR_ALL_LIBS ""
+    CACHE
+    INTERNAL
+    "Aggregation of all built libraries."
+)
+set(PXR_STATIC_LIBS ""
+    CACHE
+    INTERNAL
+    "Aggregation of all built explicitly static libraries."
+)
+set(PXR_CORE_LIBS ""
+    CACHE
+    INTERNAL
+    "Aggregation of all built core libraries."
+)
+set(PXR_OBJECT_LIBS ""
+    CACHE
+    INTERNAL
+    "Aggregation of all core libraries built as OBJECT libraries."
+)
+
+set(PXR_LIB_PREFIX "lib"
+    CACHE
+    STRING
+    "Prefix for build library name"
+)
+if (${PXR_BUILD_USD_IMAGING} AND NOT ${PXR_BUILD_IMAGING})
+    message(STATUS
+        "PXR_BUILD_IMAGING=OFF implies PXR_BUILD_USD_IMAGING=OFF"
+    )
+endif()
+if (${PXR_BUILD_EMBREE_PLUGIN} AND NOT ${PXR_BUILD_IMAGING})
+    message(STATUS
+        "PXR_BUILD_IMAGING=OFF implies PXR_BUILD_EMBREE_PLUGIN=OFF"
+    )
+endif()
+
+option(BUILD_SHARED_LIBS "Build shared libraries." ON)
+option(PXR_BUILD_MONOLITHIC "Build a monolithic library." OFF)
+set(PXR_MONOLITHIC_IMPORT ""
+    CACHE
+    STRING
+    "Path to cmake file that imports a usd_ms target"
+)
+
