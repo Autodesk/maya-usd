9574a5e2cc998d698d5fc9ca1f5bc152ca17edc1
Pal Mezei <palm@lumapictures.com>
2018-07-11 10:27:22 +1000

    Duh, TfHashMap is std::unordered map.

--- src/hdmaya/adapters/adapterRegistry.h
+++ src/hdmaya/adapters/adapterRegistry.h
@@ -2,7 +2,6 @@
 #define __HDMAYA_ADAPTER_REGISTRY_H__
 
 #include <pxr/pxr.h>
-#include <pxr/base/tf/hashmap.h>
 #include <pxr/base/tf/singleton.h>
 
 #include <maya/MFn.h>
@@ -10,6 +9,8 @@
 #include <hdmaya/delegates/delegateCtx.h>
 #include <hdmaya/adapters/dagAdapter.h>
 #include <hdmaya/adapters/lightAdapter.h>
+
+#include <unordered_map>
 
 PXR_NAMESPACE_OPEN_SCOPE
 
@@ -42,8 +43,8 @@ public:
     HDMAYA_API
     static void LoadAllPlugin();
 private:
-    TfHashMap<TfToken, DagAdapterCreator, TfToken::HashFunctor> _dagAdapters;
-    TfHashMap<TfToken, LightAdapterCreator, TfToken::HashFunctor> _lightAdapters;
+    std::unordered_map<TfToken, DagAdapterCreator, TfToken::HashFunctor> _dagAdapters;
+    std::unordered_map<TfToken, LightAdapterCreator, TfToken::HashFunctor> _lightAdapters;
 };
 
 PXR_NAMESPACE_CLOSE_SCOPE
