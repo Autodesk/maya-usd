10e550c8f67f3f956a71571cbbbdc456b64e5ba9
Fabrice Macagno <fabrice.macagno@al.com.au>
2018-07-18 18:19:49 +1000

    Fixed tests runtime environment for plugin translators

--- translators/tests/testTranslators.sh
+++ translators/tests/testTranslators.sh
@@ -5,6 +5,7 @@ export USD_LIBRARY_PATH=$2
 export MAYA_PLUG_IN_PATH=$AL_USDMAYA_LOCATION/plugin:$MAYA_PLUG_IN_PATH
 export LD_LIBRARY_PATH=$AL_USDMAYA_LOCATION/lib:$USD_LIBRARY_PATH:$LD_LIBRARY_PATH
 export PYTHONPATH=$AL_USDMAYA_LOCATION/lib/python:$USD_LIBRARY_PATH/python:$3:$PYTHONPATH
+export PXR_PLUGINPATH=$AL_USDMAYA_LOCATION/share/usd/plugins:$PXR_PLUGINPATH
 export PATH=$MAYA_LOCATION/bin:$PATH
 
 maya -batch -command "python(\"execfile(\\\"$3/testTranslators.py\\\")\")"
