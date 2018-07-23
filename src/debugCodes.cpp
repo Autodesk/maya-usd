e64e0c7b54614cdac32394db621cc395180270e5
Paul Molodowitch <pm@stanfordalumni.org>
2018-07-23 10:16:00 -0700

    more tab elimination

--- src/debugCodes.cpp
+++ src/debugCodes.cpp
@@ -7,16 +7,16 @@ PXR_NAMESPACE_OPEN_SCOPE
 TF_REGISTRY_FUNCTION(TfDebug)
 {
     TF_DEBUG_ENVIRONMENT_SYMBOL(
-    	HDMAYA_AL_CALLBACKS,
+        HDMAYA_AL_CALLBACKS,
         "Print info about the various callbacks used by hdmaya_al");
     TF_DEBUG_ENVIRONMENT_SYMBOL(
-    	HDMAYA_AL_PLUGIN,
+        HDMAYA_AL_PLUGIN,
         "Print info about the loading of the hdmaya_al plugin");
     TF_DEBUG_ENVIRONMENT_SYMBOL(
-    	HDMAYA_AL_POPULATE,
+        HDMAYA_AL_POPULATE,
         "Print info about populating the delegate from the stage");
     TF_DEBUG_ENVIRONMENT_SYMBOL(
-    	HDMAYA_AL_SELECTION,
+        HDMAYA_AL_SELECTION,
         "Print info about selecting AL objects in the maya-to-hydra viewport");
 }
 
