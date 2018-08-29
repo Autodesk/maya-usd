be335e7b0969a934937cb4ebc1a1e777ed77bcf3
Paul Molodowitch <paulm@luma-pictures.com>
2018-08-29 16:22:16 -0700

    fix for animated Transforms
    Were a few problems with animated Transform nodes:
    
    - TransformationMatrix::setPrim would reset time to the default value
    - computing the value of xform attrs would not check / update the time
    - not all xform attrs were marked as "affected by" all the time attrs
    
    Also made sure that a single compute won't trigger multiple calls to
    updateTransform (previously, this could happen if, say, both time and
    timeOffset were dirty; this was probably pretty infrequent, but with
    new changes, outTime also calls updateTransform, so it would happen
    more often without this check)

--- plugin/AL_USDMayaTestPlugin/AL/usdmaya/nodes/test_Transform.cpp
+++ plugin/AL_USDMayaTestPlugin/AL/usdmaya/nodes/test_Transform.cpp
@@ -14,17 +14,23 @@
 // limitations under the License.
 //
 #include "test_usdmaya.h"
+#include "AL/usdmaya/nodes/ProxyShape.h"
 #include "AL/usdmaya/nodes/Transform.h"
 #include "AL/usdmaya/nodes/TransformationMatrix.h"
+#include "maya/MAnimControl.h"
 #include "maya/MFileIO.h"
 #include "maya/MFnDagNode.h"
 #include "maya/MFnTransform.h"
+#include "maya/MGlobal.h"
 #include "maya/MPlug.h"
 #include "maya/MStatus.h"
 #include "maya/MTypes.h"
 
 #include "pxr/usd/usd/stage.h"
+#include "pxr/usd/usd/timeCode.h"
+#include "pxr/usd/usdGeom/xformable.h"
 
+using AL::usdmaya::nodes::ProxyShape;
 using AL::usdmaya::nodes::Transform;
 using AL::usdmaya::nodes::TransformationMatrix;
 
@@ -71,3 +77,87 @@ TEST(Transform, noInputStage)
 
   setAndCheckTranslation(4.0, 5.0, 6.0);
 }
+
+// Make sure animation data isn't lost when a Transform node exists
+// (Was a bug where m_time was incorrectly getting reset to default, resulting
+// in animation data not being read correctly)
+TEST(Transform, animationWithTransform)
+{
+  MStatus status;
+  MFileIO::newFile(true);
+  MGlobal::viewFrame(1);
+
+//  MString importCommand = "AL_usdmaya_ProxyShapeImport -connectToTime 1 -f \"" +
+  MString importCommand = "AL_usdmaya_ProxyShapeImport -f \"" +
+                          MString(AL_USDMAYA_TEST_DATA) +
+                          "/cube_moving_zaxis.usda\"";
+
+  MStringArray cmdResults;
+  status = MGlobal::executeCommand(importCommand, cmdResults, true);
+  ASSERT_TRUE(status == MStatus::kSuccess);
+  MString proxyName = cmdResults[0];
+
+  MString selectCommand = "AL_usdmaya_ProxyShapeSelect -primPath \"/pCube1\" -proxy \"" + proxyName + "\"";
+  cmdResults.clear();
+  status = MGlobal::executeCommand(selectCommand, cmdResults, true);
+  ASSERT_TRUE(status == MStatus::kSuccess);
+  MString xformName = cmdResults[0];
+
+  MSelectionList sel;
+  sel.add(proxyName);
+  sel.add(xformName);
+  MDagPath proxyDagPath;
+  MDagPath xformDagPath;
+  sel.getDagPath(0, proxyDagPath);
+  sel.getDagPath(1, xformDagPath);
+  MFnDagNode proxyMFn(proxyDagPath, &status);
+  ASSERT_TRUE(status == MStatus::kSuccess);
+  MFnDagNode xformMFn(xformDagPath, &status);
+  ASSERT_TRUE(status == MStatus::kSuccess);
+
+  auto proxy = dynamic_cast<ProxyShape*>(proxyMFn.userNode(&status));
+  ASSERT_TRUE(status == MStatus::kSuccess);
+
+  auto stage = proxy->getUsdStage();
+  ASSERT_TRUE(stage);
+  auto prim = stage->GetPrimAtPath(SdfPath("/pCube1"));
+  ASSERT_TRUE(prim.IsValid());
+
+  UsdGeomXformable xformable(prim);
+  ASSERT_TRUE(xformable);
+
+  // Make sure the time attrs are hooked up properly
+  ASSERT_FALSE(proxyMFn.findPlug("time").source().isNull());
+  ASSERT_FALSE(xformMFn.findPlug("time").source().isNull());
+
+  const auto origin = GfVec4d(0, 0, 0, 1);
+
+  auto assertTranslate = [&xformMFn, &origin, &xformable]
+                          (double expectedX, double expectedY, double expectedZ) {
+
+    ASSERT_FLOAT_EQ(xformMFn.findPlug("translateX").asDouble(), expectedX);
+    ASSERT_FLOAT_EQ(xformMFn.findPlug("translateY").asDouble(), expectedY);
+    ASSERT_FLOAT_EQ(xformMFn.findPlug("translateZ").asDouble(), expectedZ);
+
+    GfMatrix4d transform;
+    bool resetsXform;
+    xformable.GetLocalTransformation(&transform, &resetsXform,
+        UsdTimeCode(MAnimControl::currentTime().value()));
+    GfVec4d xformedPos = origin * transform;
+
+    ASSERT_FLOAT_EQ(xformedPos[0], expectedX);
+    ASSERT_FLOAT_EQ(xformedPos[1], expectedY);
+    ASSERT_FLOAT_EQ(xformedPos[2], expectedZ);
+    ASSERT_FLOAT_EQ(xformedPos[3], 1.0);
+  };
+
+  { SCOPED_TRACE(""); assertTranslate(0.0, 2.0, 0.0); }
+
+  MGlobal::viewFrame(10);
+  { SCOPED_TRACE(""); assertTranslate(0.0, 2.0, -6.7904988904413575); }
+
+  MGlobal::viewFrame(24);
+  { SCOPED_TRACE(""); assertTranslate(0.0, 2.0, -20.0); }
+}
+
+
