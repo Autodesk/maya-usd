803e90d7d3ab7ffc6b21c827bd3cb24a446d9dbd
Miguel Gao <miguel.gao@al.com.au>
2018-10-24 18:04:40 +1100

    Add test to cover the -uvs switch for export command

--- plugin/AL_USDMayaTestPlugin/AL/usdmaya/commands/test_ExportCommands.cpp
+++ plugin/AL_USDMayaTestPlugin/AL/usdmaya/commands/test_ExportCommands.cpp
@@ -20,14 +20,14 @@
 #include "maya/MFileIO.h"
 #include "maya/MFnDagNode.h"
 
-TEST(ExportCommands, exportUV)
+TEST(ExportCommands, exportUVOnly)
 {
   MFileIO::newFile(true);
 
-  const std::string temp_path = buildTempPath("AL_USDMayaTests_exportUV.usda");
+  const std::string temp_path = buildTempPath("AL_USDMayaTests_exportUVOnly.usda");
   MGlobal::executeCommand(MString("createNode transform -n geo;polyCube -n cube -cuv 2;parent cube geo;select geo"), false, true);
   MString exportCmd;
-  exportCmd.format(MString("AL_usdmaya_ExportCommand -f \"^1s\" -sl 1 -muv 1 -luv 1"), AL::maya::utils::convert(temp_path));
+  exportCmd.format(MString("AL_usdmaya_ExportCommand -f \"^1s\" -sl 1 -uvo 1 -luv 1"), AL::maya::utils::convert(temp_path));
   MGlobal::executeCommand(exportCmd, true);
 
   UsdStageRefPtr stage = UsdStage::Open(temp_path);
@@ -61,6 +61,65 @@ TEST(ExportCommands, exportUV)
     ASSERT_FLOAT_EQ(uv[0], faceUV[0]);
     ASSERT_FLOAT_EQ(uv[1], faceUV[1]);
   }
+}
+
+TEST(ExportCommands, exportUVS)
+{
+  MFileIO::newFile(true);
+
+  const std::string temp_path_withUV = buildTempPath("AL_USDMayaTests_exportUVS.usda");
+  MGlobal::executeCommand(MString("createNode transform -n geo;polyCube -n cube -cuv 2;parent cube geo;select geo"), false, true);
+  MString exportCmd;
+  exportCmd.format(MString("AL_usdmaya_ExportCommand -f \"^1s\" -sl 1 -uvs 1 -luv 1"), AL::maya::utils::convert(temp_path_withUV));
+  MGlobal::executeCommand(exportCmd, true);
+
+  UsdStageRefPtr stage = UsdStage::Open(temp_path_withUV);
+  UsdPrim geoPrim = stage->GetPrimAtPath(SdfPath("/geo"));
+  ASSERT_TRUE(geoPrim.IsValid());
+  ASSERT_EQ(geoPrim.GetSpecifier(), SdfSpecifierDef);
+
+  UsdPrim cubePrim = stage->GetPrimAtPath(SdfPath("/geo/cube"));
+  ASSERT_TRUE(cubePrim.IsValid());
+  ASSERT_EQ(cubePrim.GetSpecifier(), SdfSpecifierDef);
+
+  const uint32_t uvSZ = 24;
+  const GfVec2f faceUVs[4] = {GfVec2f(0.0f, 0.0f), GfVec2f(1.0f, 0.0f), GfVec2f(1.0f, 1.0f), GfVec2f(0.0f, 1.0f)};
+
+  UsdAttribute stAttr = cubePrim.GetAttribute(TfToken("primvars:st"));
+  ASSERT_TRUE(stAttr.IsValid());
+  VtArray<GfVec2f> uvs;
+  stAttr.Get(&uvs);
+  ASSERT_EQ(uvs.size(), uvSZ);
+
+  UsdGeomPrimvar primVar(stAttr);
+  ASSERT_TRUE(primVar.IsIndexed());
+  VtIntArray indices;
+  primVar.GetIndices(&indices);
+  ASSERT_EQ(indices.size(), uvSZ);
+
+  for (uint32_t i = 0; i < uvSZ; ++i)
+  {
+    const GfVec2f& uv =  uvs[indices[i]];
+    const GfVec2f& faceUV = faceUVs[i % 4];
+    ASSERT_FLOAT_EQ(uv[0], faceUV[0]);
+    ASSERT_FLOAT_EQ(uv[1], faceUV[1]);
+  }
+
+  // Now testing export without UV:
+  const std::string temp_path_withoutUV = buildTempPath("AL_USDMayaTests_noUVS.usda");
+  exportCmd.format(MString("AL_usdmaya_ExportCommand -f \"^1s\" -sl 1 -uvs 0 -luv 1"), AL::maya::utils::convert(temp_path_withoutUV));
+  MGlobal::executeCommand(exportCmd, true);
+
+  stage = UsdStage::Open(temp_path_withoutUV);
+  geoPrim = stage->GetPrimAtPath(SdfPath("/geo"));
+  ASSERT_TRUE(geoPrim.IsValid());
+  ASSERT_EQ(geoPrim.GetSpecifier(), SdfSpecifierDef);
+
+  cubePrim = stage->GetPrimAtPath(SdfPath("/geo/cube"));
+  ASSERT_TRUE(cubePrim.IsValid());
+  ASSERT_EQ(cubePrim.GetSpecifier(), SdfSpecifierDef);
+  stAttr = cubePrim.GetAttribute(TfToken("primvars:st"));
+  ASSERT_FALSE(stAttr.IsValid());
 }
 
 TEST(ExportCommands, extensiveAnimationCheck)
