6a376b0a64dc8ea233ea56e3d61d135aef290dc1
Hamed Sabri <hamed.sabri@autodesk.com>
2019-06-12 16:18:09 -0400

    Fix Windows build script. I was passing a wrong flag.

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ set CORE_USD_BUILD_DIRECTORY='path to core usd directory'
 set MAYA_RUNTIME='path to maya runtime'
 set INSTALL_LOCATION='path to where you want to install the plugins and libraries'
 set BUILD_TYPE='Debug, Release, RelWithDebInfo'
-set CORE_NUM='core#'
+set CORE_NUM=%NUMBER_OF_PROCESSORS%
 
 set MAYA_USD_BUILD_DIRECTORY=%cd%
 if not exist "build" mkdir build
@@ -19,7 +19,7 @@ cmake .. -G "Visual Studio 15 2017 Win64" ^
 -DUSD_LOCATION_OVERRIDE=%CORE_USD_BUILD_DIRECTORY% ^
 -DPXR_BUILD_MAYA_PLUGIN=TRUE 
 
-cmake --build . --config %BUILD_TYPE% --target install -j %CORE_NUM%
+cmake --build . --config %BUILD_TYPE% --target install -- /m:%CORE_NUM%
 
 endlocal
 exit /b %errorlevel%
