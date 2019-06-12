bc45d74aea7a751424c3499ae95e4cdc24e0c2c3
Hamed Sabri <hamed.sabri@autodesk.com>
2019-06-12 15:15:12 -0400

    - Clean up build scripts and make sure to pass --config when building using cmake.

--- build.bat
+++ build.bat
@@ -1,19 +1,25 @@
 rem @echo off
 setlocal
 
+rem variables to be set
 set CORE_USD_BUILD_DIRECTORY='path to core usd directory'
+set MAYA_RUNTIME='path to maya runtime'
+set INSTALL_LOCATION='path to where you want to install the plugins and libraries'
+set BUILD_TYPE='Debug, Release, RelWithDebInfo'
+set CORE_NUM='core#'
 
 set MAYA_USD_BUILD_DIRECTORY=%cd%
 if not exist "build" mkdir build
-cd %MAYA_USD_BUILD_DIRECTORY%\build
+cd %MAYA_USD_BUILD_DIRECTORY%/build
 
-cmake -G "Visual Studio 15 2017 Win64" .. ^
--DCMAKE_BUILD_TYPE=RelWithDebInfo ^
--DCMAKE_INSTALL_PREFIX='path to where you want to install the plugins and libraries' ^
+cmake .. -G "Visual Studio 15 2017 Win64" ^
+-DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+-DCMAKE_INSTALL_PREFIX=%INSTALL_LOCATION% ^
+-DMAYA_LOCATION=%MAYA_RUNTIME% ^
 -DUSD_LOCATION_OVERRIDE=%CORE_USD_BUILD_DIRECTORY% ^
--DPXR_BUILD_MAYA_PLUGIN=TRUE ^
--DBUILD_MAYAUSD_CORE=FALSE ^
--DBUILD_MAYAUSD_PLUGIN=FALSE
+-DPXR_BUILD_MAYA_PLUGIN=TRUE 
+
+cmake --build . --config %BUILD_TYPE% --target install -j %CORE_NUM%
 
 endlocal
 exit /b %errorlevel%
--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
+# variables to be set
 export CORE_USD_BUILD_DIRECTORY='path to core usd directory'
+export MAYA_RUNTIME='path to maya runtime'
+export INSTALL_LOCATION='path to where you want to install the plugins and libraries'
+export BUILD_TYPE='Debug, Release, RelWithDebInfo'
+export CORE_NUM='core#'
 
 export MAYA_USD_BUILD_DIRECTORY=$PWD
 if [[ ! -e build ]]; then
@@ -8,10 +14,10 @@ fi
 cd $MAYA_USD_BUILD_DIRECTORY/build
 
 cmake .. \
--DCMAKE_BUILD_TYPE=RelWithDebInfo \
--DCMAKE_INSTALL_PREFIX='path to where you want to install the plugins and libraries' \
+-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+-DCMAKE_INSTALL_PREFIX=$INSTALL_LOCATION \
+-DMAYA_LOCATION=$MAYA_RUNTIME \
 -DUSD_LOCATION_OVERRIDE=$CORE_USD_BUILD_DIRECTORY \
--DMAYA_LOCATION='path to maya runtime' \
--DPXR_BUILD_MAYA_PLUGIN=TRUE \
+-DPXR_BUILD_MAYA_PLUGIN=TRUE 
 
-cmake --build . --target install -j 'core#'
+cmake --build . --config $BUILD_TYPE --target install -j $CORE_NUM
