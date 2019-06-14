f2c26a4cc3f5d24321636607e45d0703b94bf225
Hamed Sabri <hamed.sabri@autodesk.com>
2019-06-14 16:09:45 -0400

    - Added MAYA_DEVKIT_LOCATION to the build scripts. For now, enabled CMAKE_WANT_UFE_BUILD. (WIP)

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ setlocal
 rem variables to be set
 set CORE_USD_BUILD_DIRECTORY='path to core usd directory'
 set MAYA_RUNTIME='path to maya runtime'
+set MAYA_DEVKIT_LOCATION='path to maya devkit'
 set INSTALL_LOCATION='path to where you want to install the plugins and libraries'
 set BUILD_TYPE='Debug, Release, RelWithDebInfo'
 set CORE_NUM=%NUMBER_OF_PROCESSORS%
@@ -19,6 +20,7 @@ cmake .. -G "Visual Studio 15 2017 Win64" ^
 -DUSD_LOCATION_OVERRIDE=%CORE_USD_BUILD_DIRECTORY% ^
 -DUSD_CONFIG_FILE=%CORE_USD_BUILD_DIRECTORY%/pxrConfig.cmake ^
 -DBOOST_ROOT=%CORE_USD_BUILD_DIRECTORY% ^
+-DMAYA_DEVKIT_LOCATION=%MAYA_DEVKIT_LOCATION% ^
 -DBUILD_USDMAYA_PXR_TRANSLATORS=FALSE
 
 cmake --build . --config %BUILD_TYPE% --target install -- /m:%CORE_NUM%
--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # variables to be set
 export CORE_USD_BUILD_DIRECTORY='path to core usd directory'
 export MAYA_RUNTIME='path to maya runtime'
+export MAYA_DEVKIT_LOCATION='path to maya devkit'
 export INSTALL_LOCATION='path to where you want to install the plugins and libraries'
 export BUILD_TYPE='Debug, Release, RelWithDebInfo'
 export CORE_NUM='core#'
@@ -20,6 +21,7 @@ cmake .. \
 -DUSD_LOCATION_OVERRIDE=$CORE_USD_BUILD_DIRECTORY \
 -DUSD_CONFIG_FILE=$CORE_USD_BUILD_DIRECTORY/pxrConfig.cmake \
 -DBOOST_ROOT=$CORE_USD_BUILD_DIRECTORY \
+-DMAYA_DEVKIT_LOCATION=$MAYA_DEVKIT_LOCATION \
 -DBUILD_USDMAYA_PXR_TRANSLATORS=FALSE
 
 cmake --build . --config $BUILD_TYPE --target install -j $CORE_NUM
