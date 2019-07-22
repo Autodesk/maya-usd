4c9631b7563f97a790ae21686b919123aaa38713
Hamed Sabri <hamed.sabri@autodesk.com>
2019-07-22 16:03:44 -0400

    - Updated license header text in(*.h, *.cpp, *.py) (#62)

--- cmake/usd.cmake
+++ cmake/usd.cmake
@@ -1,14 +1,21 @@
 #-
-#*****************************************************************************
+# =======================================================================
 # Copyright 2019 Autodesk, Inc. All rights reserved.
 #
-# These coded instructions, statements, and computer programs contain
-# unpublished proprietary information written by Autodesk, Inc. and are
-# protected by Federal copyright law. They may not be disclosed to third
-# parties or copied or duplicated in any form, in whole or in part, without
-# the prior written consent of Autodesk, Inc.
-#*****************************************************************************
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =======================================================================
 #+
+
 
 function(init_usd)
     set(PXR_USD_LOCATION "${PXR_USD_LOCATION}" PARENT_SCOPE)
