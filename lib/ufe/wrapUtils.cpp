8fe2cf46abbc394ac1e9d9b7366d04743db3a1fc
Hamed Sabri <hamed.sabri@autodesk.com>
2019-09-30 11:47:33 -0400

    Guard getNodeNameFromRawItem function against UFE2.

--- lib/ufe/wrapUtils.cpp
+++ lib/ufe/wrapUtils.cpp
@@ -19,39 +19,45 @@ using namespace boost::python;
 
 UsdPrim getPrimFromRawItem(uint64_t rawItem)
 {
-	Ufe::SceneItem* item = reinterpret_cast<Ufe::SceneItem*>(rawItem);
-	ufe::UsdSceneItem* usdItem = dynamic_cast<ufe::UsdSceneItem*>(item);
-	if (nullptr != usdItem)
-	{
-		return usdItem->prim();
-	}
-	return UsdPrim();
+    Ufe::SceneItem* item = reinterpret_cast<Ufe::SceneItem*>(rawItem);
+    ufe::UsdSceneItem* usdItem = dynamic_cast<ufe::UsdSceneItem*>(item);
+    if (nullptr != usdItem)
+    {
+        return usdItem->prim();
+    }
+    return UsdPrim();
 }
 
+#ifdef UFE_V2_FEATURES_AVAILABLE
 std::string getNodeNameFromRawItem(uint64_t rawItem)
 {
-	std::string name;
-	Ufe::SceneItem* item = reinterpret_cast<Ufe::SceneItem*>(rawItem);
-	if (nullptr != item)
-		name = item->nodeName();
-	return name;
+    std::string name;
+    Ufe::SceneItem* item = reinterpret_cast<Ufe::SceneItem*>(rawItem);
+    if (nullptr != item)
+        name = item->nodeName();
+    return name;
 }
+#endif
 
 std::string getNodeTypeFromRawItem(uint64_t rawItem)
 {
-	std::string type;
-	Ufe::SceneItem* item = reinterpret_cast<Ufe::SceneItem*>(rawItem);
-	if (nullptr != item)
-	{
-		// Prepend the name of the runtime manager of this item to the type.
-		type = Ufe::RunTimeMgr::instance().getName(item->runTimeId()) + item->nodeType();
-	}
-	return type;
+    std::string type;
+    Ufe::SceneItem* item = reinterpret_cast<Ufe::SceneItem*>(rawItem);
+    if (nullptr != item)
+    {
+        // Prepend the name of the runtime manager of this item to the type.
+        type = Ufe::RunTimeMgr::instance().getName(item->runTimeId()) + item->nodeType();
+    }
+    return type;
 }
 
 BOOST_PYTHON_MODULE(ufe)
 {
-	def("getPrimFromRawItem", getPrimFromRawItem);
-	def("getNodeNameFromRawItem", getNodeNameFromRawItem);
-	def("getNodeTypeFromRawItem", getNodeTypeFromRawItem);
+    def("getPrimFromRawItem", getPrimFromRawItem);
+    
+    #ifdef UFE_V2_FEATURES_AVAILABLE
+        def("getNodeNameFromRawItem", getNodeNameFromRawItem);
+    #endif
+
+    def("getNodeTypeFromRawItem", getNodeTypeFromRawItem);
 }
