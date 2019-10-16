277f4cf731fb262fa2e31bfb3d0e7d1a57d6405a
sabrih <hamed.sabri@autodesk.com>
2019-10-15 22:42:15 -0400

    - added support to replace old luma lics to new autodesk lics - renamed both to all

--- replace_lic.py
+++ replace_lic.py
@@ -13,12 +13,14 @@ THIS_FILE = os.path.normpath(os.path.abspath(inspect.getsourcefile(lambda: None)
 
 parser = argparse.ArgumentParser(description=__doc__)
 mode_group = parser.add_mutually_exclusive_group()
-mode_group.add_argument('--mode', choices=['pxr', 'al', 'both'], default='both',
+mode_group.add_argument('--mode', choices=['pxr', 'al', 'luma', 'all'], default='all',
     help="Which repo we're trying to replace lics for")
 mode_group.add_argument('--al', help='Replace al lics',
     action='store_const', const='al', dest='mode')
 mode_group.add_argument('--pxr', help='Replace pixar lics',
     action='store_const', const='pxr', dest='mode')
+mode_group.add_argument('--luma', help='Replace luma lics',
+    action='store_const', const='luma', dest='mode')
 
 args = parser.parse_args()
 
@@ -107,6 +109,26 @@ new_py_lic = """#
 #
 """
 
+old_luma_cpp_lic = """// Licensed under the Apache License, Version 2.0 (the "Apache License")
+// with the following modification you may not use this file except in
+// compliance with the Apache License and the following modification to it:
+// Section 6. Trademarks. is deleted and replaced with:
+//
+// 6. Trademarks. This License does not grant permission to use the trade
+//    names, trademarks, service marks, or product names of the Licensor
+//    and its affiliates, except as required to comply with Section 4(c) of
+//    the License and to reproduce the content of the NOTICE file.
+//
+// You may obtain a copy of the Apache License at
+//
+//     http:#www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the Apache License with the above modification is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the Apache License for the specific
+// language governing permissions and limitations under the Apache License."""
+
 old_al_cpp_lic_line = """// you may not use this file except in compliance with the License.//"""
 new_cpp_lic_line = """// you may not use this file except in compliance with the License."""
 
@@ -125,13 +147,18 @@ elif args.mode == 'al':
         (old_pxr_py_lic, new_py_lic),
         (old_al_py_lic_line, new_py_lic_line),
     ]
-elif args.mode == 'both':
+elif args.mode == 'luma':
+    replacement_pairs = [
+        (old_luma_cpp_lic, new_cpp_lic),
+    ]
+elif args.mode == 'all':
     replacement_pairs = [
         (old_pxr_cpp_lic, new_cpp_lic),
         (old_pxr_py_lic, new_py_lic),
         (old_al_cpp_lic_line, new_cpp_lic_line),
         (old_al_py_lic, new_py_lic),
         (old_al_py_lic_line, new_py_lic_line),
+        (old_luma_cpp_lic, new_cpp_lic),
     ]
 else:
     raise ValueError("Unrecognized mode: {}".format(args.mode))
