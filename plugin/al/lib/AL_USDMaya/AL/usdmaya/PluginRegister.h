d0c677bf0f2a7a1af324bc22dfb693ce3a132649
eoinm <eoinm@al.com.au>
2019-10-28 15:46:17 +1100

    added option var to prevent 'constructLockPrims' from running when selecting transforms (AL PR #970)

--- plugin/al/lib/AL_USDMaya/AL/usdmaya/PluginRegister.h
+++ plugin/al/lib/AL_USDMaya/AL/usdmaya/PluginRegister.h
@@ -209,6 +209,11 @@ MStatus registerPlugin(AFnPlugin& plugin)
     MGlobal::setOptionVarValue("AL_usdmaya_pushToPrim", false);
   }
 
+  if(!MGlobal::optionVarExists("AL_usdmaya_ignoreLockPrims"))
+  {
+    MGlobal::setOptionVarValue("AL_usdmaya_ignoreLockPrims", false);
+  }
+
   MStatus status;
 
   // gpuCachePluginMain used as an example.
@@ -309,6 +314,7 @@ MStatus registerPlugin(AFnPlugin& plugin)
   AL::maya::utils::MenuBuilder::addEntry("USD/Animated Geometry/Connect selected meshes to USD (animated)", "AL_usdmaya_meshAnimImport");
   AL::maya::utils::MenuBuilder::addEntry("USD/Selection Enabled", "optionVar -iv \\\"AL_usdmaya_selectionEnabled\\\" #1", true, MGlobal::optionVarIntValue("AL_usdmaya_selectionEnabled"));
   AL::maya::utils::MenuBuilder::addEntry("USD/Enable pushToPrim", "optionVar -iv \\\"AL_usdmaya_pushToPrim\\\" #1", true, MGlobal::optionVarIntValue("AL_usdmaya_pushToPrim"));
+  AL::maya::utils::MenuBuilder::addEntry("USD/Selection Ignore Lock Prims Enabled", "optionVar -iv \\\"AL_usdmaya_ignoreLockPrims\\\" #1", true, MGlobal::optionVarIntValue("AL_usdmaya_ignoreLockPrims"));
   CHECK_MSTATUS(AL::maya::utils::MenuBuilder::generatePluginUI(plugin, "AL_usdmaya"));
   AL::usdmaya::Global::onPluginLoad();
 
