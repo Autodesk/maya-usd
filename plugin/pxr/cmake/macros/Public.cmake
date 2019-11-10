cb22d51cbd4baf40bcbda0aa000378a6e4032661
sabrih <hamed.sabri@autodesk.com>
2019-11-10 18:21:20 -0500

    disable setting of custom output lib prefix via PXR_LIB_PREFIX
    instead, always use ${CMAKE_SHARED_LIBRARY_PREFIX} to find the
    appropriate prefix for shared libraries on the current platform

--- plugin/pxr/cmake/macros/Public.cmake
+++ plugin/pxr/cmake/macros/Public.cmake
@@ -125,7 +125,7 @@ function(pxr_library NAME)
             endif()
         endif()
 
-        set(prefix "${PXR_LIB_PREFIX}")
+        set(prefix "${CMAKE_SHARED_LIBRARY_PREFIX}")
         if(args_TYPE STREQUAL "STATIC")
             set(suffix ${CMAKE_STATIC_LIBRARY_SUFFIX})
         else()
@@ -690,7 +690,6 @@ function(pxr_toplevel_prologue)
             set_target_properties(usd_ms
                 PROPERTIES
                     FOLDER "${folder}"
-                    PREFIX "${PXR_LIB_PREFIX}"
             )
             _get_install_dir("lib" libInstallPrefix)
             install(
@@ -824,7 +823,6 @@ function(pxr_monolithic_epilogue)
         PROPERTIES
             FOLDER "${folder}"
             POSITION_INDEPENDENT_CODE ON
-            PREFIX "${PXR_LIB_PREFIX}"
     )
 
     # Adding $<TARGET_OBJECTS:foo> will not bring along compile
