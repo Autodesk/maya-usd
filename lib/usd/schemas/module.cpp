8036e23f5a7099292bd9aa4a8de5c27fbc35b367
Krystian Ligenza <krystian.ligenza@autodesk.com>
2020-02-04 04:49:20 -0500

    Add python bindings

--- lib/usd/schemas/module.cpp
+++ lib/usd/schemas/module.cpp
@@ -14,20 +14,12 @@
 // limitations under the License.
 //
 
-#include "pxr/base/tf/registryManager.h"
-#include "pxr/base/tf/scriptModuleLoader.h"
-#include "pxr/base/tf/token.h"
+#include "pxr/base/tf/pyModule.h"
 
-#include <vector>
+PXR_NAMESPACE_USING_DIRECTIVE
 
-PXR_NAMESPACE_OPEN_SCOPE
-
-TF_REGISTRY_FUNCTION(TfScriptModuleLoader) {
-    const std::vector<TfToken> reqs = {
-        TfToken("usd"),      TfToken("usdGeom")
-    };
-    TfScriptModuleLoader::GetInstance().
-        RegisterLibrary(TfToken("schemas"), TfToken("mayaUsd.schemas"), reqs);
+TF_WRAP_MODULE
+{
+    TF_WRAP(MayaUsd_SchemasTokens);
+    TF_WRAP(AL_usd_MayaReference);
 }
-
-PXR_NAMESPACE_CLOSE_SCOPE
