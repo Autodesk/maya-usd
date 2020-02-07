823aab8cc8ac85a31d1b017b3306a9308799003e
Matt Johnson <mrjz@umich.edu>
2020-02-06 18:15:09 -0800

    add a MProfiler category for UsdMayaGLBatchRenderer
    This category will be used to track viewport performance when using Pixar's
    "batch renderer" Hydra integration.

--- lib/render/pxrUsdMayaGL/batchRenderer.cpp
+++ lib/render/pxrUsdMayaGL/batchRenderer.cpp
@@ -94,6 +94,14 @@ TF_DEFINE_PRIVATE_TOKENS(
 
 TF_INSTANTIATE_SINGLETON(UsdMayaGLBatchRenderer);
 
+const int UsdMayaGLBatchRenderer::ProfilerCategory = MProfiler::addCategory(
+#if MAYA_API_VERSION >= 20190000
+    "UsdMayaGLBatchRenderer", "UsdMayaGLBatchRenderer"
+#else
+    "UsdMayaGLBatchRenderer"
+#endif
+);
+
 /* static */
 void
 UsdMayaGLBatchRenderer::Init()
