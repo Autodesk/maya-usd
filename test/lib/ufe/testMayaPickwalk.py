9d8167da411846e40196c67a3b898f337a71932f
Sean Donnelly <23455376+seando-adsk@users.noreply.github.com>
2020-04-24 09:25:38 -0400

    MAYA-104203 - USD AE: undo/redo UFE rename command will crash maya if AE is open
    * Some changes in Maya (related to UFE) required updating
      a couple of the UFE tests here.

--- test/lib/ufe/testMayaPickwalk.py
+++ test/lib/ufe/testMayaPickwalk.py
@@ -168,8 +168,8 @@ class MayaUFEPickWalkTesting(unittest.TestCase):
         
         # Pickwalk on unsupported UFE items
         cmds.select("pCube1.e[6]")
-        self.snapShotAndTest((["pCube1.e[6]"], ["pCubeShape1"]))
-        self.assertTrue(next(iter(ufe.GlobalSelection.get())).isProperty())
+        self.snapShotAndTest((["pCube1.e[6]"], []))
+        self.assertTrue(ufe.GlobalSelection.get().empty())
         
         # TODO: HS 2019 : test fails.  MAYA-101373
 #        cmds.pickWalk(type="edgeloop", d="right")
