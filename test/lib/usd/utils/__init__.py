fd610329a21df6faf48dd4202ff0bd8be6418aa2
Rob Bateman <Rob.Bateman@al.com.au>
2020-05-19 08:13:50 +1000

    adding  in transform API from develop

---
+++ test/lib/usd/utils/__init__.py
@@ -0,0 +1,16 @@
+from pxr import Tf
+import _mayaUsdUtils
+Tf.PrepareModule(_mayaUsdUtils, locals())
+del Tf
+
+try:
+    import __DOC
+    __DOC.Execute(locals())
+    del __DOC
+except Exception:
+    try:
+        import __tmpDoc
+        __tmpDoc.Execute(locals())
+        del __tmpDoc
+    except:
+        pass
---
+++ test/lib/usd/utils/test_MayaTransformAPI.py
@@ -0,0 +1,140 @@
+
+from mayaUsdUtils import MayaTransformAPI, RotationOrder
+from pxr import Usd, Sdf, Gf, UsdGeom
+import unittest
+
+
+class TestMayaTransformAPI(unittest.TestCase):
+
+    def test_scale(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.scale(Gf.Vec3f(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeScale)
+
+    def test_translate(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.translate(Gf.Vec3d(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+
+    def test_rotateAxis(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotateAxis(Gf.Vec3f(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateXYZ)
+
+    def test_scalePivot(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.scalePivot(Gf.Vec3f(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 2)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+        self.assertTrue(ops[1].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+
+    def test_rotatePivot(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotatePivot(Gf.Vec3f(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 2)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+        self.assertTrue(ops[1].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+
+    def test_scalePivotTranslate(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.scalePivotTranslate(Gf.Vec3f(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+
+    def test_rotatePivotTranslate(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotatePivotTranslate(Gf.Vec3f(2,3,4), Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeTranslate)
+
+    def test_rotateXYZ(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotate(Gf.Vec3f(2,3,4), RotationOrder.kXYZ, Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateXYZ)
+
+    def test_rotateXZY(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotate(Gf.Vec3f(2,3,4), RotationOrder.kXZY, Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateXZY)
+
+    def test_rotateYXZ(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotate(Gf.Vec3f(2,3,4), RotationOrder.kYXZ, Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateYXZ)
+
+    def test_rotateYZX(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotate(Gf.Vec3f(2,3,4), RotationOrder.kYZX, Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateYZX)
+
+    def test_rotateZXY(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotate(Gf.Vec3f(2,3,4), RotationOrder.kZXY, Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateZXY)
+
+    def test_rotateZYX(self):
+        stage = Usd.Stage.CreateInMemory()
+        xform = UsdGeom.Xform.Define(stage, "/pCube1")
+        api = MayaTransformAPI(xform.GetPrim(), False)
+        self.assertTrue(api.isValid())
+        api.rotate(Gf.Vec3f(2,3,4), RotationOrder.kZYX, Usd.TimeCode.Default())
+        ops = xform.GetOrderedXformOps()
+        self.assertTrue(len(ops) == 1)
+        self.assertTrue(ops[0].GetOpType() == UsdGeom.XformOp.TypeRotateZYX)
+        
