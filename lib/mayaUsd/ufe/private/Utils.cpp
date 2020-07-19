f94d20a44b926f197eb0c36ad081f8680ea8c4a8
Hamed Sabri <hamed.sabri@autodesk.com>
2020-07-19 19:53:05 -0400

    Create a utility function for applying restrict command rules.

--- lib/mayaUsd/ufe/private/Utils.cpp
+++ lib/mayaUsd/ufe/private/Utils.cpp
@@ -23,6 +23,8 @@
 #include <pxr/base/tf/stringUtils.h>
 #include <pxr/usd/usdGeom/xformable.h>
 
+#include <mayaUsdUtils/util.h>
+
 PXR_NAMESPACE_USING_DIRECTIVE
 
 MAYAUSD_NS_DEF {
@@ -123,6 +125,47 @@ UsdGeomXformCommonAPI convertToCompatibleCommonAPI(const UsdPrim& prim)
 		}
 	}
 	return primXform;
+}
+
+void applyCommandRestriction(const UsdPrim& prim, const std::string& commandName)
+{
+    // early check to see if a particular node has any specs to contribute
+    // to the final composed prim. e.g (a node in payload)
+    if(!MayaUsdUtils::hasSpecs(prim)){
+        std::string err = TfStringPrintf("Cannot %s [%s] because it doesn't have any specs to contribute to the composed prim.",
+                                          commandName, 
+                                          prim.GetName().GetString().c_str());
+        throw std::runtime_error(err.c_str());
+    }
+
+    // if the current layer doesn't have any contributions
+    if (!MayaUsdUtils::doesEditTargetLayerContribute(prim)) {
+        auto strongestContributingLayer = MayaUsdUtils::strongestContributingLayer(prim);
+        std::string err = TfStringPrintf("Cannot %s [%s] defined on another layer. " 
+                                         "Please set [%s] as the target layer to proceed", 
+                                         commandName, 
+                                         prim.GetName().GetString().c_str(),
+                                         strongestContributingLayer->GetDisplayName().c_str());
+        throw std::runtime_error(err.c_str());
+    }
+    else
+    {
+        auto layers = MayaUsdUtils::layersWithContribution(prim);
+        // if we have more than 2 layers that contributes to the final composed prim
+        if (layers.size() > 1) {
+            std::string layerDisplayNames;
+            for (auto layer : layers) {
+                layerDisplayNames.append("[" + layer->GetDisplayName() + "]" + ",");
+            }
+            layerDisplayNames.pop_back();
+            std::string err = TfStringPrintf("Cannot %s [%s] with definitions or opinions on other layers. "
+                                             "Opinions exist in %s", 
+                                             commandName,
+                                             prim.GetName().GetString().c_str(), 
+                                             layerDisplayNames.c_str());
+            throw std::runtime_error(err.c_str());
+        }
+    }
 }
 
 //------------------------------------------------------------------------------
