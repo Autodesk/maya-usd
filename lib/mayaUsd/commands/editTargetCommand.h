3436cc08b220ea899b6fc6f06f1ba04aa78274c1
Luc-Eric Rousseau <Luc-Eric.Rousseau@autodesk.com>
2020-07-20 11:39:12 -0400

    MAYA-104967 Undo/Redo USD target change and sublayer edits for Maya USD Layer Editor

---
+++ lib/mayaUsd/commands/editTargetCommand.h
@@ -0,0 +1,55 @@
+//
+// Copyright 2020 Autodesk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <mayaUsd/mayaUsd.h>
+
+#include <maya/MPxCommand.h>
+#include <maya/MString.h>
+
+#include <memory>
+#include <string>
+
+namespace MAYAUSD_NS {
+
+namespace Impl {
+class SetEditTarget;
+}
+
+class EditTargetCommand : public MPxCommand {
+public:
+    // plugin registration requirements
+    static const char commandName[];
+    static void*      creator();
+    static MSyntax    createSyntax();
+
+    // MPxCommand callbacks
+    MStatus doIt(const MArgList& argList) override;
+    MStatus undoIt() override;
+    MStatus redoIt() override;
+    bool    isUndoable() const override;
+
+private:
+    MStatus parseArgs(const MArgList& argList);
+
+    enum class Mode { kCreate, kEdit, kQuery } _cmdMode = Mode::kCreate;
+    bool isEdit() const { return _cmdMode == Mode::kEdit; }
+    bool isQuery() const { return _cmdMode == Mode::kQuery; }
+
+    MString                              _proxyShapePath;
+    std::unique_ptr<Impl::SetEditTarget> _setEditTarget;
+};
+
+} // namespace MAYAUSD_NS
