7006feb800a97a93638ddf0f4ac97980306cb2a3
Matt Johnson <mrjz@umich.edu>
2020-10-15 17:40:01 -0700

    add testing to ensure that UsdPreviewSurface "useSpecularWorkflow" converts and round-trips correctly

--- test/lib/usd/translators/testUsdExportImportRoundtripPreviewSurface.py
+++ test/lib/usd/translators/testUsdExportImportRoundtripPreviewSurface.py
@@ -67,6 +67,7 @@ class testUsdExportImportRoundtripPreviewSurface(unittest.TestCase):
         cmds.setAttr(material_node + ".roughness", 0.25)
         cmds.setAttr(material_node + ".specularColor", 0.125, 0.25, 0.75,
                      type="double3")
+        cmds.setAttr(material_node + ".useSpecularWorkflow", True)
 
         file_node = cmds.shadingNode("file", asTexture=True,
                                      isColorManaged=True)
@@ -131,6 +132,7 @@ class testUsdExportImportRoundtripPreviewSurface(unittest.TestCase):
                                0.25)
         self.assertEqual(cmds.getAttr("usdPreviewSurface2.specularColor"),
                          [(0.125, 0.25, 0.75)])
+        self.assertTrue(cmds.getAttr("usdPreviewSurface2.useSpecularWorkflow"))
         self.assertEqual(cmds.getAttr("file2.defaultColor"),
                          [(0.5, 0.25, 0.125)])
         original_path = cmds.getAttr(file_node+".fileTextureName")
