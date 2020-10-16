f8e4670a9322e32aa442a23acd9b471826f06e47
JGamache-autodesk <jerry.gamache@autodesk.com>
2020-10-16 16:45:15 -0400

    MAYA-106205 - Updated the MtoH vendor and version

--- lib/mayaUsd/render/mayaToHydra/plugin.cpp
+++ lib/mayaUsd/render/mayaToHydra/plugin.cpp
@@ -29,6 +29,13 @@
 #include "renderOverride.h"
 #include "viewCommand.h"
 
+#if defined(MAYAUSD_VERSION)
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#else
+#error "MAYAUSD_VERSION is not defined"
+#endif
+
 PXR_NAMESPACE_USING_DIRECTIVE
 
 PLUGIN_EXPORT MStatus initializePlugin(MObject obj) {
@@ -46,7 +53,7 @@ PLUGIN_EXPORT MStatus initializePlugin(MObject obj) {
     snprintf(envVarData.data(), envVarSize, "%s", envVarSet);
     putenv(envVarData.data());
 
-    MFnPlugin plugin(obj, "Luma Pictures", "2018", "Any");
+    MFnPlugin plugin(obj, "Autodesk", TOSTRING(MAYAUSD_VERSION), "Any");
 
     if (!plugin.registerCommand(
             MtohViewCmd::name, MtohViewCmd::creator,
@@ -69,7 +76,7 @@ PLUGIN_EXPORT MStatus initializePlugin(MObject obj) {
 }
 
 PLUGIN_EXPORT MStatus uninitializePlugin(MObject obj) {
-    MFnPlugin plugin(obj, "Luma Pictures", "2018", "Any");
+    MFnPlugin plugin(obj, "Autodesk", TOSTRING(MAYAUSD_VERSION), "Any");
     MStatus ret = MS::kSuccess;
 
     auto* renderer = MHWRender::MRenderer::theRenderer();
