72a17e9bc6b4b7ad833ef92c84868e8a04d7f8b4
Krystian Ligenza <krystian.ligenza@autodesk.com>
2020-10-16 08:22:31 -0400

    Fix warning as error - signed/unsigned mismatch

--- lib/mayaUsd/fileio/utils/readUtil.cpp
+++ lib/mayaUsd/fileio/utils/readUtil.cpp
@@ -152,9 +152,9 @@ _FindOrCreateMayaNumericAttr(
         }
 
         const unsigned int numChildren = MFnCompoundAttribute(attrObj).numChildren();
-        if(numChildren < 4) {
+        if(numChildren < 4u) {
             static MString suffix[4] = {" X", " Y", " Z", " W"};
-            for(int i = 0; i < numChildren; i++) {
+            for(unsigned int i = 0; i < numChildren; i++) {
                 MFnAttribute(attr.child(i)).setNiceNameOverride(niceName+suffix[i]);
             }
         } else {
