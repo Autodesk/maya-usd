c42fd90bf7ec57d8d7350fd3a7dc0652e1bde33e
Sean Donnelly <23455376+seando-adsk@users.noreply.github.com>
2020-12-03 12:01:27 -0500

    MAYA-108214 - Attributes categorized by schema
    * New "usdschemabase" template which is at top level
      of the schema inheritance scheme so all prim types
      will use it. All the node type templates are
      deprecated and will be removed in the future.

--- test/lib/ufe/testDuplicateCmd.py
+++ test/lib/ufe/testDuplicateCmd.py
@@ -189,7 +189,7 @@ class DuplicateCmdTestCase(unittest.TestCase):
         cmds.undo()  # undo duplication
         cmds.undo()  # undo deletion
 
-    @unittest.skipUnless(mayaUtils.previewReleaseVersion() >= 122, 'Requires Maya fixes only available in Maya Preview Release 122 or later.')
+    @unittest.skipUnless(mayaUtils.previewReleaseVersion() >= 121, 'Requires Maya fixes only available in Maya Preview Release 121 or later.')
     def testSmartTransformDuplicate(self):
         '''Test smart transform option of duplicate command.'''
         torusFile = mayaUtils.getTestScene("groupCmd", "torus.usda")
