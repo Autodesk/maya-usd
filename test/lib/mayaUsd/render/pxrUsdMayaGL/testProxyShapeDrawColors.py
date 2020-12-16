eceec31bc3bd80ec631629a2b81daba07fa5b183
Paul Molodowitch <paulm@lumapictures.com>
2020-12-16 13:46:45 -0800

    [tests] interactive tests ensure output streams are flushed before quitting

--- test/lib/mayaUsd/render/pxrUsdMayaGL/testProxyShapeDrawColors.py
+++ test/lib/mayaUsd/render/pxrUsdMayaGL/testProxyShapeDrawColors.py
@@ -119,15 +119,4 @@ class testProxyShapeDrawColors(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(
-        testProxyShapeDrawColors)
-
-    results = unittest.TextTestRunner(stream=sys.__stderr__).run(suite)
-    if results.wasSuccessful():
-        exitCode = 0
-    else:
-        exitCode = 1
-    # maya running interactively often absorbs all the output.  comment out the
-    # following to prevent maya from exiting and open the script editor to look
-    # at failures.
-    cmds.quit(abort=True, exitCode=exitCode)
+    fixturesUtils.runTests(globals())
