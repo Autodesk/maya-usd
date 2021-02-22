fe35686aec2f83bdfe74f8aeef980d9bcb9ba259
Pierre Tremblay <pierre.tremblay@autodesk.com>
2021-02-22 15:01:41 -0500

    Fix clang-format error.

--- lib/mayaUsd/ufe/UsdTransform3dMatrixOp.cpp
+++ lib/mayaUsd/ufe/UsdTransform3dMatrixOp.cpp
@@ -86,7 +86,8 @@ GfMatrix4d xformInv(
 
     GfMatrix4d m { 1 };
     if (!UsdGeomXformable::GetLocalTransformation(&m, ops, getTime(path))) {
-        TF_FATAL_ERROR("Local transformation computation for item %s failed.", path.string().c_str());
+        TF_FATAL_ERROR(
+            "Local transformation computation for item %s failed.", path.string().c_str());
     }
 
     return m.GetInverse();
