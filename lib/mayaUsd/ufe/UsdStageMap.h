89fcd585d40ef5f18e4defe7aefda8817651ce81
Pierre Tremblay <pierre.tremblay@autodesk.com>
2021-03-10 16:11:08 -0500

    Minimize name-based lookup of proxy shape nodes.

--- lib/mayaUsd/ufe/UsdStageMap.h
+++ lib/mayaUsd/ufe/UsdStageMap.h
@@ -27,33 +27,31 @@
 
 #include <unordered_map>
 
-// Allow for use of MObjectHandle with std::unordered_map.
-namespace std {
-template <> struct hash<MObjectHandle>
-{
-    std::size_t operator()(const MObjectHandle& handle) const
-    {
-        return static_cast<std::size_t>(handle.hashCode());
-    }
-};
-} // namespace std
+// Pending rework of mayaUsd namespaces, MayaUsdProxyShapeBase is in the Pixar
+// namespace.  PPT, 9-Mar-2021.
+PXR_NAMESPACE_OPEN_SCOPE
+class MayaUsdProxyShapeBase;
+PXR_NAMESPACE_CLOSE_SCOPE
 
 namespace MAYAUSD_NS_DEF {
 namespace ufe {
 
 //! \brief USD Stage Map
 /*!
-        Two-way map of proxy shape UFE path to corresponding stage.
+    Two-way map of proxy shape UFE path to corresponding stage.
 
-        We will assume that	a USD proxy shape will not be instanced (even though
-        nothing in the data model prevents it).  To support renaming and repathing,
-        we store an MObjectHandle in the maps, which is invariant to renaming and
-        repathing, and compute the path on access.  This is slower than a scheme
-        where we cache using the Ufe::Path, but such a cache must be refreshed on
-        rename and repath, which is non-trivial, since there is no guarantee on the
-        order of notification of Ufe observers.  An earlier implementation with
-        rename observation had the Maya Outliner (which observes rename) access the
-        UsdStageMap on rename before the UsdStageMap had been updated.
+    We will assume that	a USD proxy shape will not be instanced (even though
+    nothing in the data model prevents it).  To generalized access to the
+    underlying node, we store an MObjectHandle in the maps.
+
+    The cache is refreshed on access to a stage given a path which cannot be
+    found.  In this way, the cache does not need to observe the Maya data
+    model, and we avoid order of notification problems where one observer would
+    need to access the cache before it is refreshed, since there is no
+    guarantee on the order of notification of Ufe observers.  An earlier
+    implementation with rename observation had the Maya Outliner (which
+    observes rename) access the UsdStageMap on rename before the UsdStageMap
+    had been updated.
 */
 class MAYAUSD_CORE_PUBLIC UsdStageMap
 {
@@ -69,8 +67,16 @@ public:
     UsdStageMap(UsdStageMap&&) = delete;
     UsdStageMap& operator=(UsdStageMap&&) = delete;
 
-    //! Get USD stage corresponding to argument Maya Dag path.
+    //! Get USD stage for the first segment of the argument path.
     PXR_NS::UsdStageWeakPtr stage(const Ufe::Path& path);
+
+    //! Return the ProxyShape object for the first segment of the argument
+    //! path.  If no such proxy shape exists, returns a null MObject.
+    MObject proxyShape(const Ufe::Path& path);
+
+    //! Return the ProxyShape node for the first segment of the argument path.
+    //! If no such proxy shape node exists, returns a null pointer.
+    PXR_NS::MayaUsdProxyShapeBase* proxyShapeNode(const Ufe::Path& path);
 
     //! Return the ProxyShape node UFE path for the argument stage.
     Ufe::Path path(PXR_NS::UsdStageWeakPtr stage);
@@ -86,14 +92,14 @@ public:
     bool isDirty() const { return fDirty; }
 
 private:
-    void addItem(const Ufe::Path& path, PXR_NS::UsdStageWeakPtr stage);
+    void addItem(const Ufe::Path& path);
     void rebuildIfDirty();
 
 private:
     // We keep two maps for fast lookup when there are many proxy shapes.
-    using ObjectToStage = std::unordered_map<MObjectHandle, PXR_NS::UsdStageWeakPtr>;
+    using PathToObject = std::unordered_map<Ufe::Path, MObjectHandle>;
     using StageToObject = PXR_NS::TfHashMap<PXR_NS::UsdStageWeakPtr, MObjectHandle, PXR_NS::TfHash>;
-    ObjectToStage fObjectToStage;
+    PathToObject  fPathToObject;
     StageToObject fStageToObject;
     bool          fDirty { true };
 
