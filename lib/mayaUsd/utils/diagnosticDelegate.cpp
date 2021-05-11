23a4dd37275a993a53142a98b885f873b4130109
Hamed Sabri <hamed.sabri@autodesk.com>
2021-05-11 11:06:13 -0400

    filter the file path.

--- lib/mayaUsd/utils/diagnosticDelegate.cpp
+++ lib/mayaUsd/utils/diagnosticDelegate.cpp
@@ -67,13 +67,17 @@ class _WarningOnlyDelegate : public UsdUtilsCoalescingDiagnosticDelegate
 
 static MString _FormatDiagnostic(const TfDiagnosticBase& d)
 {
+    // strip out all the characters before "maya-usd"
+    std::string filterFilePath { d.GetContext().GetFile() };
+    filterFilePath = filterFilePath.substr(filterFilePath.find("maya-usd"));
+
     const std::string msg = TfStringPrintf(
         "%s -- %s in %s at line %zu of %s",
         d.GetCommentary().c_str(),
         TfDiagnosticMgr::GetCodeName(d.GetDiagnosticCode()).c_str(),
         d.GetContext().GetFunction(),
         d.GetContext().GetLine(),
-        d.GetContext().GetFile());
+        filterFilePath.c_str());
 
     return TfGetEnvSetting(MAYAUSD_SHOW_FULL_DIAGNOSTICS) ? msg.c_str() : d.GetCommentary().c_str();
 }
