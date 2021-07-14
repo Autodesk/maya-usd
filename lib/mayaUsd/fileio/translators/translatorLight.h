24602e31a8c27148cedaf545450d72f2a9a090d6
ADS\blaines <sebastien.blaineau.ortega@autodesk.com>
2021-07-14 15:34:20 +0200

    MAYA-112777 - Add support for light translators

---
+++ lib/mayaUsd/fileio/translators/translatorLight.h
@@ -0,0 +1,77 @@
+//
+// Copyright 2016 Pixar
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef PXRUSDMAYA_TRANSLATOR_LIGHT_H
+#define PXRUSDMAYA_TRANSLATOR_LIGHT_H
+
+#include <mayaUsd/base/api.h>
+#include <mayaUsd/fileio/primReaderArgs.h>
+#include <mayaUsd/fileio/primReaderContext.h>
+#include <mayaUsd/fileio/primWriterArgs.h>
+#include <mayaUsd/fileio/primWriterContext.h>
+#include <mayaUsd/fileio/utils/writeUtil.h>
+#include <pxr/usd/usdLux/light.h>
+#include <pxr/usd/usdLux/distantLight.h>
+#include <pxr/usd/usdLux/sphereLight.h>
+#include <pxr/usd/usdLux/rectLight.h>
+#include <pxr/pxr.h>
+
+#include <maya/MFnDirectionalLight.h>
+#include <maya/MFnSpotLight.h>
+#include <maya/MFnPointLight.h>
+#include <maya/MFnAreaLight.h>
+
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+/// \brief Provides helper functions for translating to/from UsdLux
+struct UsdMayaTranslatorLight
+{
+    /// Exports the UsdLuxLight schema attributes from a generic Maya MFnLight.
+    /// This function should be called when exporting to any of the specialized light schemas.
+    /// Return true if all the parameters were exported properly.
+    MAYAUSD_CORE_PUBLIC
+    static bool WriteLightAttrs(const UsdTimeCode &usdTime, UsdLuxLight &usdLight, 
+            MFnLight &mayaLight, UsdUtilsSparseValueWriter* valueWriter = nullptr);
+
+    /// Exports Maya's directional light attributes using UsdLuxDistantLight schema
+    MAYAUSD_CORE_PUBLIC
+    static bool WriteDirectionalLightAttrs(const UsdTimeCode &usdTime, UsdLuxDistantLight &usdLight, 
+            MFnDirectionalLight &mayaLight, UsdUtilsSparseValueWriter* valueWriter = nullptr);
+
+    /// Exports Maya's point light attributes using UsdLuxSphereLight schema
+    MAYAUSD_CORE_PUBLIC
+    static bool WritePointLightAttrs(const UsdTimeCode &usdTime, UsdLuxSphereLight &usdLight, 
+            MFnPointLight &mayaLight, UsdUtilsSparseValueWriter* valueWriter = nullptr);
+
+    /// Exports Maya's spot light attributes using UsdLuxSphereLight and UsdLuxShapingAPI schemas
+    MAYAUSD_CORE_PUBLIC
+    static bool WriteSpotLightAttrs(const UsdTimeCode &usdTime, UsdLuxSphereLight &usdLight, 
+            MFnSpotLight &mayaLight, UsdUtilsSparseValueWriter* valueWriter = nullptr);
+
+    /// Exports Maya's area light attributes using UsdLuxRectLight schema
+    MAYAUSD_CORE_PUBLIC
+    static bool WriteAreaLightAttrs(const UsdTimeCode &usdTime, UsdLuxRectLight &usdLight, 
+            MFnAreaLight &mayaLight, UsdUtilsSparseValueWriter* valueWriter = nullptr);
+
+    /// Import a UsdLuxLight schema as a corresponding Maya light.
+    /// Return true if the maya light was properly created and imported
+    MAYAUSD_CORE_PUBLIC
+    static bool Read(const UsdMayaPrimReaderArgs& args, UsdMayaPrimReaderContext* context);
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif
---
+++ test/lib/usd/translators/testUsdImportLight.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env mayapy
+#
+# Copyright 2017 Pixar
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import unittest
+
+from pxr import Gf
+from pxr import Usd
+
+from maya import OpenMaya
+from maya import OpenMayaAnim
+from maya import cmds
+from maya import standalone
+
+import fixturesUtils
+
+class testUsdImportLight(unittest.TestCase):
+
+    START_TIMECODE = 1.0
+    END_TIMECODE = 5.0
+
+    @classmethod
+    def setUpClass(cls):
+        inputPath = fixturesUtils.readOnlySetUpClass(__file__)
+        cmds.file(new=True, force=True)
+
+        # Import from USD.
+        usdFilePath = os.path.join(inputPath, "UsdImportLightTest", "LightsTest.usda")
+        cmds.usdImport(file=usdFilePath, readAnimData=True)
+        cls._stage = Usd.Stage.Open(usdFilePath)
+
+    @classmethod
+    def tearDownClass(cls):
+        standalone.uninitialize()
+
+    def testStageOpens(self):
+        """
+        Tests that the USD stage being imported opens successfully.
+        """
+        self.assertTrue(self._stage)
+        self.assertEqual(self._stage.GetStartTimeCode(), self.START_TIMECODE)
+        self.assertEqual(self._stage.GetEndTimeCode(), self.END_TIMECODE)
+
+    def _GetMayaDependencyNode(self, objectName):
+         selectionList = OpenMaya.MSelectionList()
+         selectionList.add(objectName)
+         mObj = OpenMaya.MObject()
+         selectionList.getDependNode(0, mObj)
+
+         depNodeFn = OpenMaya.MFnDependencyNode(mObj)
+         self.assertTrue(depNodeFn)
+
+         return depNodeFn
+   
+    def _ValidateMayaDistantLight(self):
+        nodePath = 'directionalLight1'        
+        depNodeFn = self._GetMayaDependencyNode(nodePath)
+        self.assertTrue(depNodeFn)
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.intensity' % nodePath),
+            2, 1e-6))
+        
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorR' % nodePath),
+            1, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorG' % nodePath),
+            0.9, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorB' % nodePath),
+            0.8, 1e-6))
+        
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateX' % nodePath),
+            -20, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateY' % nodePath),
+            -40, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateZ' % nodePath),
+            0, 1e-6))
+
+        shadowColorList = cmds.getAttr('%s.shadowColor' % nodePath)
+        self.assertTrue(Gf.IsClose(shadowColorList[0],
+            Gf.Vec3f(0.1, 0.2, 0.3), 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.lightAngle' % nodePath),
+            1.5, 1e-6))       
+        # verify the animation is imported properly, check at frame 5
+        cmds.currentTime(5)
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.lightAngle' % nodePath),
+            2, 1e-6))
+        cmds.currentTime(1)
+
+    def _ValidateMayaPointLight(self):
+        nodePath = 'pointLight1'
+        depNodeFn = self._GetMayaDependencyNode(nodePath)
+        cmds.currentTime(1)
+        self.assertTrue(cmds.getAttr('%s.emitDiffuse' % nodePath) == 1)
+        self.assertTrue(cmds.getAttr('%s.emitSpecular' % nodePath) == 0)
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateX' % nodePath),
+            -10, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateY' % nodePath),
+            10, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateZ' % nodePath),
+            0, 1e-6))
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorR' % nodePath),
+            1, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorG' % nodePath),
+            0.5, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorB' % nodePath),
+            0.1, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.intensity' % nodePath),
+            0.5, 1e-6))
+        # verify the animation is imported properly, check at frame 5
+        cmds.currentTime(5)
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.intensity' % nodePath),
+            2, 1e-6))
+        cmds.currentTime(1)
+        
+    def _ValidateMayaSpotLight(self):
+        nodePath = 'spotLight1'
+        depNodeFn = self._GetMayaDependencyNode(nodePath)
+        
+        self.assertTrue(cmds.getAttr('%s.emitDiffuse' % nodePath) == 0)
+        self.assertTrue(cmds.getAttr('%s.emitSpecular' % nodePath) == 1)
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateX' % nodePath),
+            10, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateY' % nodePath),
+            7, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateZ' % nodePath),
+            -8, 1e-6))
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateX' % nodePath),
+            -45, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateY' % nodePath),
+            90, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateZ' % nodePath),
+            -5, 1e-6))
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorR' % nodePath),
+            0.3, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorG' % nodePath),
+            1, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorB' % nodePath),
+            0.2, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.intensity' % nodePath),
+            0.8, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.dropoff' % nodePath),
+            8, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.coneAngle' % nodePath),
+            30, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.penumbraAngle' % nodePath),
+            10, 1e-6))
+        # verify the animation is imported properly, check at frame 5
+        cmds.currentTime(5)
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorR' % nodePath),
+            0, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorG' % nodePath),
+            0.2, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorB' % nodePath),
+            0.1, 1e-6))
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateX' % nodePath),
+            5, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateY' % nodePath),
+            3, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateZ' % nodePath),
+            8, 1e-6))
+        cmds.currentTime(1)
+
+    def _ValidateMayaAreaLight(self):
+        nodePath = 'areaLight1'
+        depNodeFn = self._GetMayaDependencyNode(nodePath)
+        
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateX' % nodePath),
+            8, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateY' % nodePath),
+            0, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.translateZ' % nodePath),
+            10, 1e-6))
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateX' % nodePath),
+            0, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateY' % nodePath),
+            23, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.rotateZ' % nodePath),
+            0, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.scaleX' % nodePath),
+            4, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.scaleY' % nodePath),
+            3, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.scaleZ' % nodePath),
+            2, 1e-6))
+
+
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorR' % nodePath),
+            0.8, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorG' % nodePath),
+            0.7, 1e-6))
+        self.assertTrue(Gf.IsClose(cmds.getAttr('%s.colorB' % nodePath),
+            0.6, 1e-6))
+        self.assertTrue(cmds.getAttr('%s.normalize' % nodePath) == 0)
+   
+
+    def testImportMayaLights(self):
+        """
+        Tests that UsdLux schema USD prims import into Maya as the appropriate Maya light.
+        """                
+        self._ValidateMayaDistantLight()
+        self._ValidateMayaPointLight()
+        self._ValidateMayaSpotLight()
+        self._ValidateMayaAreaLight()
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
