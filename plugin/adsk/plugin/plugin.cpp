d76f8ca0d540ef7644a9da948321a63151f0bc9f
Pierre Baillargeon <Pierre.Baillargeon@autodesk.com>
2021-08-09 10:49:29 -0400

    MAYA112769 move plugInfo.json to render library.

--- plugin/adsk/plugin/plugin.cpp
+++ plugin/adsk/plugin/plugin.cpp
@@ -322,11 +322,6 @@ MStatus initializePlugin(MObject obj)
     UsdMayaSceneResetNotice::InstallListener();
     UsdMayaDiagnosticDelegate::InstallDelegate();
 
-
-    MString pluginPath = plugin.loadPath(&status);
-    pluginPath += "/plugInfo.json";
-    PlugRegistry::GetInstance().RegisterPlugins(pluginPath.asChar());
-
     return status;
 }
 
