4987a65f6e1cb73186a067bee0978bdb879620fb
Pierre Baillargeon <Pierre.Baillargeon@autodesk.com>
2021-11-11 17:10:15 -0500

    MAYA-114707 separate in multiple sub-tests

--- test/lib/ufe/testSelectionByArray.py
+++ test/lib/ufe/testSelectionByArray.py
@@ -29,6 +29,24 @@ import ufe
 import unittest
 
 
+def melSelectCmd(paths):
+    """
+    Create the mel command to select the given objects by passing them as a single array.
+    """
+    cmd = '''string $obj[];'''
+    for i, path in enumerate(paths):
+        cmd = cmd + '$obj[%s] = "%s";' % (i, path)
+    cmd = cmd + 'select $obj;'
+    return cmd
+
+
+def melSelect(paths):
+    """
+    Call the mel select command using a single array as argument.
+    """
+    mel.eval(melSelectCmd(paths))
+
+    
 class SelectByArrayTestCase(unittest.TestCase):
     '''Verify UFE selection on a USD scene by passing argument in arrays.'''
 
@@ -62,66 +80,84 @@ class SelectByArrayTestCase(unittest.TestCase):
             return ufe.Path([shapeSegment, usdUtils.createUfePathSegment(name)])
 
         ufeNames = ["/cubeXform", "/cylinderXform", "/sphereXform"]
-        self.usdPaths = [makeUsdPath(name) for name in ufeNames]
+        self.usdPaths = [ufe.PathString.string(makeUsdPath(name)) for name in ufeNames]
         self.mayaPaths = ["pCube1", "pCylinder1", "pSphere1"]
 
         # Clear selection to start off
         cmds.select(clear=True)
 
     #@unittest.skipUnless(mayaUtils.mayaMajorVersion >= 2023, 'testSelectByArray only works with fixes available in Maya 2023.')
-    def testSelectByArray(self):
+    def testSelectMayaPathInMel(self):
         """
-        Select multiple items by passing them in an array.
-        Perform the test using Python and using mel.
+        Select multiple Maya items by passing them in an array to a mel command.
         """
-        def melSelectCmd(paths):
-            cmd = '''string $obj[];'''
-            for i, path in enumerate(paths):
-                cmd = cmd + '$obj[%s] = "%s";' % (i, path)
-            cmd = cmd + 'select $obj;'
-            return cmd
-
-        def melSelect(paths):
-            mel.eval(melSelectCmd(paths))
-
-        # Test using mel commands.
-
         cmds.select(clear=True)
         melSelect(self.mayaPaths)
 
         sn = ufe.GlobalSelection.get()
         self.assertEqual(len(sn), 3)
 
-        print(self.usdPaths)
+    #@unittest.skipUnless(mayaUtils.mayaMajorVersion >= 2023, 'testSelectByArray only works with fixes available in Maya 2023.')
+    def testSelectUFEInMel(self):
+        """
+        Select multiple UFE items by passing them in an array to a mel command.
+        """
         cmds.select(clear=True)
         melSelect(self.usdPaths)
 
         sn = ufe.GlobalSelection.get()
         self.assertEqual(len(sn), 3)
 
+    #@unittest.skipUnless(mayaUtils.mayaMajorVersion >= 2023, 'testSelectByArray only works with fixes available in Maya 2023.')
+    def testSelectUFEAndMayaInMel(self):
+        """
+        Select a mix of Maya and UFE items by passing them in an array to a mel command.
+        """
+        interleaved = []
+        for ab in zip(self.mayaPaths, self.usdPaths):
+            interleaved.append(ab[0])
+            interleaved.append(ab[1])
+
         cmds.select(clear=True)
-        melSelect(self.mayaPaths + self.usdPaths)
+        melSelect(interleaved)
 
         sn = ufe.GlobalSelection.get()
         self.assertEqual(len(sn), 6)
 
-        # Test using Python commands.
-
+    #@unittest.skipUnless(mayaUtils.mayaMajorVersion >= 2023, 'testSelectByArray only works with fixes available in Maya 2023.')
+    def testSelectMayaPathInPython(self):
+        """
+        Select multiple Maya items by passing them in an array to a Python command.
+        """
         cmds.select(clear=True)
         cmds.select(self.mayaPaths)
 
         sn = ufe.GlobalSelection.get()
         self.assertEqual(len(sn), 3)
 
-        print(self.usdPaths)
+    #@unittest.skipUnless(mayaUtils.mayaMajorVersion >= 2023, 'testSelectByArray only works with fixes available in Maya 2023.')
+    def testSelectUFEInPython(self):
+        """
+        Select multiple UFE items by passing them in an array to a Python command.
+        """
         cmds.select(clear=True)
         cmds.select(self.usdPaths)
 
         sn = ufe.GlobalSelection.get()
         self.assertEqual(len(sn), 3)
 
+    #@unittest.skipUnless(mayaUtils.mayaMajorVersion >= 2023, 'testSelectByArray only works with fixes available in Maya 2023.')
+    def testSelectUFEAndMayaInPython(self):
+        """
+        Select a mix of Maya and UFE items by passing them in an array to a Python command.
+        """
+        interleaved = []
+        for ab in zip(self.mayaPaths, self.usdPaths):
+            interleaved.append(ab[0])
+            interleaved.append(ab[1])
+
         cmds.select(clear=True)
-        cmds.select(self.mayaPaths + self.usdPaths)
+        cmds.select(interleaved)
 
         sn = ufe.GlobalSelection.get()
         self.assertEqual(len(sn), 6)
