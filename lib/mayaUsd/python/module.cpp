064d9b544c443f49dac476b443dbb3bd87aae295
Yves Boudreault <Yves.Boudreault@autodesk.com>
2021-12-08 09:14:42 -0500

    MAYA-114925 Expose python bindings for UsdMayaPrimUpdater

--- lib/mayaUsd/python/module.cpp
+++ lib/mayaUsd/python/module.cpp
@@ -33,6 +33,9 @@ TF_WRAP_MODULE
     TF_WRAP(DiagnosticDelegate);
     TF_WRAP(MeshWriteUtils);
 #ifdef UFE_V3_FEATURES_AVAILABLE
+    TF_WRAP(PrimUpdater);
+    TF_WRAP(PrimUpdaterArgs);
+    TF_WRAP(PrimUpdaterContext);
     TF_WRAP(PrimUpdaterManager);
 #endif
     TF_WRAP(Query);
