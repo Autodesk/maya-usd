e4a6f8361da38af8cdc7376d9e52964b3dffc151
Jerry Gamache <jerry.gamache@autodesk.com>
2022-02-15 09:39:11 -0500

    Fix copyright year

--- test/lib/usd/translators/testUsdImportExportSiteSpecificConfig.py
+++ test/lib/usd/translators/testUsdImportExportSiteSpecificConfig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env mayapy
 #
-# Copyright 2021 Autodesk
+# Copyright 2022 Autodesk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
