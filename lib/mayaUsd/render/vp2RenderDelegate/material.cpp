63f702424249534c39db9ff425c885f71780b9ee
Jerry Gamache <jerry.gamache@autodesk.com>
2022-02-21 12:29:27 -0500

    USD 22.03 integration: API change for MaterialX
    And extra parameter was added to HdMtlxCreateMtlxDocumentFromHdNetwork

--- lib/mayaUsd/render/vp2RenderDelegate/material.cpp
+++ lib/mayaUsd/render/vp2RenderDelegate/material.cpp
@@ -1949,6 +1949,9 @@ MHWRender::MShaderInstance* HdVP2Material::_CreateMaterialXShaderInstance(
             mtlxDoc = HdMtlxCreateMtlxDocumentFromHdNetwork(
                 fixedNetwork,
                 *surfTerminal, // MaterialX HdNode
+#if PXR_VERSION > 2111
+                fixedPath,
+#endif
                 SdfPath(_mtlxTokens->USD_Mtlx_VP2_Material),
                 _GetMaterialXData()._mtlxLibrary,
                 &hdTextureNodes,
