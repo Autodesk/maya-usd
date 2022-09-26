bcaf0f42bf230775f4997290abc7bf86395ff792
alicedegirolamo <alicedg94@outlook.it>
2022-09-26 11:16:26 +0200

    Add missing clang format

--- lib/mayaUsd/ufe/UsdUndoAttributesCommands.h
+++ lib/mayaUsd/ufe/UsdUndoAttributesCommands.h
@@ -66,7 +66,7 @@ public:
 
 private:
     const Ufe::Path            _sceneItemPath;
-    std::string          _name;
+    std::string                _name;
     const Ufe::Attribute::Type _type;
 }; // UsdAddAttributeCommand
 
