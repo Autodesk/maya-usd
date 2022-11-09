0454724479434ad495ccb494e46a07ede2d20536
Stefan Minning <stefan.minning@autodesk.com>
2022-11-09 11:02:37 +0100

    Introduce Material Interface, unit tests, fixes for code review

---
+++ lib/mayaUsd/ufe/UsdMaterialHandler.cpp
@@ -0,0 +1,56 @@
+// ===========================================================================
+// Copyright 2022 Autodesk, Inc. All rights reserved.
+//
+// Use of this software is subject to the terms of the Autodesk license
+// agreement provided at the time of installation or download, or which
+// otherwise accompanies this software in either electronic or hard copy form.
+// ===========================================================================
+
+#include "UsdMaterialHandler.h"
+
+#include <mayaUsd/ufe/UsdSceneItem.h>
+
+#include <pxr/base/tf/diagnostic.h>
+#include <pxr/usd/usdGeom/imageable.h>
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+namespace MAYAUSD_NS_DEF {
+namespace ufe {
+
+UsdMaterialHandler::UsdMaterialHandler()
+    : Ufe::MaterialHandler()
+{
+}
+
+UsdMaterialHandler::~UsdMaterialHandler() { }
+
+/*static*/
+UsdMaterialHandler::Ptr UsdMaterialHandler::create()
+{
+    return std::make_shared<UsdMaterialHandler>();
+}
+
+//------------------------------------------------------------------------------
+// UsdMaterialHandler overrides
+//------------------------------------------------------------------------------
+
+Ufe::Material::Ptr UsdMaterialHandler::material(const Ufe::SceneItem::Ptr& item) const
+{
+    UsdSceneItem::Ptr usdItem = std::dynamic_pointer_cast<UsdSceneItem>(item);
+    if (!TF_VERIFY(usdItem)) {
+        return nullptr;
+    }
+
+    // Test if this item is imageable. If not, then we cannot create a material
+    // interface for it, which is a valid case (such as for a material node type).
+    PXR_NS::UsdGeomImageable primSchema(usdItem->prim());
+    if (!primSchema) {
+        return nullptr;
+    }
+
+    return UsdMaterial::create(usdItem);
+}
+
+} // namespace ufe
+} // namespace MAYAUSD_NS_DEF
---
+++ lib/mayaUsd/ufe/UsdMaterialHandler.h
@@ -0,0 +1,47 @@
+// ===========================================================================
+// Copyright 2022 Autodesk, Inc. All rights reserved.
+//
+// Use of this software is subject to the terms of the Autodesk license
+// agreement provided at the time of installation or download, or which
+// otherwise accompanies this software in either electronic or hard copy form.
+// ===========================================================================
+#pragma once
+
+#include <mayaUsd/base/api.h>
+#include <mayaUsd/ufe/UsdMaterial.h>
+
+#include <ufe/materialHandler.h>
+
+namespace MAYAUSD_NS_DEF {
+namespace ufe {
+
+//! \brief USD run-time Material handler.
+/*!
+        Factory object for Material interfaces.
+ */
+class MAYAUSD_CORE_PUBLIC UsdMaterialHandler : public Ufe::MaterialHandler
+{
+public:
+    typedef std::shared_ptr<UsdMaterialHandler> Ptr;
+
+    UsdMaterialHandler();
+    ~UsdMaterialHandler() override;
+
+    //@{
+    //! Delete the copy/move constructors assignment operators.
+    UsdMaterialHandler(const UsdMaterialHandler&) = delete;
+    UsdMaterialHandler& operator=(const UsdMaterialHandler&) = delete;
+    UsdMaterialHandler(UsdMaterialHandler&&) = delete;
+    UsdMaterialHandler& operator=(UsdMaterialHandler&&) = delete;
+    //@}
+
+    //! Create a UsdMaterialHandler.
+    static UsdMaterialHandler::Ptr create();
+
+    // UsdMaterialHandler overrides
+    Ufe::Material::Ptr material(const Ufe::SceneItem::Ptr& item) const override;
+
+}; // UsdMaterialHandler
+
+} // namespace ufe
+} // namespace MAYAUSD_NS_DEF
