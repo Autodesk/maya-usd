e043af662df766d9043908a7407bf7f5d974a5db
Ivan Vlasov <ivan.vlasov@autodesk.com>
2023-02-02 12:56:04 -0500

    Trying to fix another preflight failure

--- lib/mayaUsd/python/wrapUtil.cpp
+++ lib/mayaUsd/python/wrapUtil.cpp
@@ -55,5 +55,5 @@ void wrapUtil()
                   .def("getDictionaryFromEncodedOptions", getDictionaryFromEncodedOptions)
                   .def(
                       "getPathRelativeToMayaSceneFile",
-                      &UsdMayaUtilFileSystem::getPathRelativeToMayaSceneFile);
+                      UsdMayaUtilFileSystem::getPathRelativeToMayaSceneFile);
 }
