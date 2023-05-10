bd26eb848fa602677e442b9c14d9e3023f3da446
Pierre Baillargeon <Pierre.Baillargeon@autodesk.com>
2023-05-10 14:40:58 -0400

    MAYA-129169 correctly duplicate prim
    Correctly handle all these case:
    - Simple prim with a single opinion: simple copy of source layer into destination layer.
    - Prim with opinion in multiple local layers: marge all opinion in destination layer.
    - Prim containing a USD reference: keep the reference but do not copy the reference contents into the destination layer.
    - Prim containing a USD payload: keep the payload but do not copy the payload contents into the destination layer.
    - Prim inside a reference or payload: copy the reference or payload contents in the destination layer.
    
    The code changes are:
    - Move layer-related function from the geneic util.h header and C++ file to the more specific utils/layers.h header and C++ file.
    - Make the getLocalPrimStack function available publicly.
    - Add getPrimStackForLayers function to find where a prim has opinions in a setof layers.
    - Add a description for the USD implementation of the UFE duplicate command.
    - It explains that the command copies the local opinions of the prim where it is defined.
    - Add a unit test that verify that duplicating a prim containing a USD reference does not copy the contents of the reference in the copy.
    - Remove the LayerAndPath type and replace it with PrimSpecHandle.
    - Prim spec contain the necessary data, and more, so it is more versatile.
    - Change some function names to be clearer.

--- lib/mayaUsd/utils/layers.cpp
+++ lib/mayaUsd/utils/layers.cpp
@@ -16,6 +16,7 @@
 #include "layers.h"
 
 #include <pxr/usd/pcp/layerStack.h>
+#include <pxr/usd/usd/primCompositionQuery.h>
 
 #include <deque>
 
@@ -116,44 +117,16 @@ void enforceMutedLayer(const PXR_NS::UsdPrim& prim, const char* command)
     }
 }
 
-static SdfPrimSpecHandleVector _GetLocalPrimStack(const UsdPrim& prim)
-{
-    SdfPrimSpecHandleVector primSpecs;
-
-    UsdStagePtr stage = prim.GetStage();
-    if (!stage)
-        return primSpecs;
-
-    // The goal is to avoid editing non-local layers. This issue is,
-    // for example, that a rename operation would fail when applied
-    // to a prim that references a show asset because the rename operation
-    // would be attempted on the reference and classes it inherits.
-    //
-    // Concrete example:
-    //     - Create a test asset that inherits from one or more classes
-    //     - Create a prim within a Maya Usd scene that references this asset
-    //     - Attempt to rename the prim
-    //     - Observe the failure due to Sdf policy
-
-    for (const SdfLayerHandle& layer : stage->GetLayerStack()) {
-        const SdfPrimSpecHandle primSpec = layer->GetPrimAtPath(prim.GetPath());
-        if (primSpec)
-            primSpecs.push_back(primSpec);
-    }
-
-    return primSpecs;
-}
-
 void applyToAllPrimSpecs(const UsdPrim& prim, const PrimSpecFunc& func)
 {
-    const SdfPrimSpecHandleVector primStack = _GetLocalPrimStack(prim);
+    const SdfPrimSpecHandleVector primStack = getLocalPrimStack(prim);
     for (const SdfPrimSpecHandle& spec : primStack)
         func(prim, spec);
 }
 
 void applyToAllLayersWithOpinions(const UsdPrim& prim, PrimLayerFunc& func)
 {
-    const SdfPrimSpecHandleVector primStack = _GetLocalPrimStack(prim);
+    const SdfPrimSpecHandleVector primStack = getLocalPrimStack(prim);
     for (const SdfPrimSpecHandle& spec : primStack) {
         const auto layer = spec->GetLayer();
         func(prim, layer);
@@ -165,7 +138,7 @@ void applyToSomeLayersWithOpinions(
     const std::set<SdfLayerRefPtr>& layers,
     PrimLayerFunc&                  func)
 {
-    const SdfPrimSpecHandleVector primStack = _GetLocalPrimStack(prim);
+    const SdfPrimSpecHandleVector primStack = getLocalPrimStack(prim);
     for (const SdfPrimSpecHandle& spec : primStack) {
         const auto layer = spec->GetLayer();
         if (layers.count(layer) == 0)
@@ -241,6 +214,167 @@ SdfLayerHandle getStrongerLayer(
     // layer hierarchy because we don't want to let opinions that are owned by
     // the application interfere with the user commands.
     return getStrongerLayer(stage->GetRootLayer(), layer1, layer2);
+}
+
+SdfPrimSpecHandleVector
+getPrimStackForLayers(const UsdPrim& prim, const SdfLayerHandleVector& layers)
+{
+    SdfPrimSpecHandleVector primSpecs;
+
+    for (const SdfLayerHandle& layer : layers) {
+        const SdfPrimSpecHandle primSpec = layer->GetPrimAtPath(prim.GetPath());
+        if (primSpec)
+            primSpecs.push_back(primSpec);
+    }
+
+    return primSpecs;
+}
+
+SdfPrimSpecHandleVector getLocalPrimStack(const UsdPrim& prim)
+{
+    // The goal is to avoid editing non-local layers. This issue is,
+    // for example, that a rename operation would fail when applied
+    // to a prim that references a show asset because the rename operation
+    // would be attempted on the reference and classes it inherits.
+    //
+    // Concrete example:
+    //     - Create a test asset that inherits from one or more classes
+    //     - Create a prim within a Maya Usd scene that references this asset
+    //     - Attempt to rename the prim
+    //     - Observe the failure due to Sdf policy
+
+    UsdStagePtr stage = prim.GetStage();
+    if (!stage)
+        return {};
+
+    return getPrimStackForLayers(prim, stage->GetLayerStack());
+}
+
+static void addSubLayers(const SdfLayerHandle& layer, std::set<SdfLayerHandle>& layers)
+{
+    if (!layer)
+        return;
+
+    if (layers.count(layer) > 0)
+        return;
+
+    layers.insert(layer);
+    for (const std::string& layerPath : layer->GetSubLayerPaths())
+        addSubLayers(SdfLayer::FindOrOpen(layerPath), layers);
+}
+
+static bool hasSubLayerInSet(const SdfLayerHandle& layer, const std::set<SdfLayerHandle>& layers)
+{
+    if (!layer)
+        return false;
+
+    for (const std::string& layerPath : layer->GetSubLayerPaths())
+        if (layers.count(SdfLayer::FindOrOpen(layerPath)) > 0)
+            return true;
+
+    return false;
+}
+
+SdfPrimSpecHandleVector getDefiningPrimStack(const UsdPrim& prim)
+{
+    UsdStagePtr stage = prim.GetStage();
+    if (!stage)
+        return {};
+
+    const SdfPrimSpecHandle defPrimSpec = getDefiningPrimSpec(prim);
+    if (!defPrimSpec)
+        return {};
+
+    // Simple case: the prim is defined in the local layer stack of the stage.
+    {
+        const SdfLayerHandle          defLayer = defPrimSpec->GetLayer();
+        const SdfPrimSpecHandleVector primSpecsInStageLayers = getLocalPrimStack(prim);
+        for (const SdfPrimSpecHandle& primSpec : primSpecsInStageLayers)
+            if (primSpec->GetLayer() == defLayer)
+                return primSpecsInStageLayers;
+    }
+
+    // Complex case: the prim is defined within a reference or payload.
+    //
+    // We need to build the layer stack of that payload or reference.
+    // Note that it could be a reference inside a reference, or a payload
+    // in a reference, or any deeper such nesting.
+    //
+    // We build the defining prim stack by going outward from the defining
+    // prim spec. We keep other prim spec if their layer is a parent or child
+    // of the layer that defines the prim. (The code beow starts from all the
+    // prim specs and removes the ones that are not in the layer hierarchy
+    // above and below the defining layer.)
+
+    // This keep tracks of layers we know are in the defining layer stack.
+    // We use this to identify other layer, for example identify a parent
+    // layer if one of its children in in this set.
+    std::set<SdfLayerHandle> definingLayers;
+    addSubLayers(defPrimSpec->GetLayer(), definingLayers);
+
+    SdfPrimSpecHandleVector primStack = prim.GetPrimStack();
+
+    const auto defPrimSpecPos = std::find(primStack.begin(), primStack.end(), defPrimSpec);
+    if (defPrimSpecPos == primStack.end())
+        return {};
+
+    const size_t defPrimSpecIndex = defPrimSpecPos - primStack.begin();
+
+    // Remove the sub-layers that are not in the local stack of the defining layer.
+    for (size_t index = defPrimSpecIndex + 1; index < primStack.size(); index += 1) {
+        const SdfPrimSpecHandle primSpec = primStack[index];
+
+        // If the prim spec layer is a sub-layer of the defining layer, then we keep
+        // it and add its children to the defining layers set.
+        SdfLayerHandle layer = primSpec->GetLayer();
+        if (definingLayers.count(layer) > 0) {
+            addSubLayers(layer, definingLayers);
+            continue;
+        }
+
+        // Otherwise, we remove the prim spec from the defining prim stack and
+        // decrease the index, since we erase it from the vector and the loop
+        // will increase the index.
+        primStack.erase(primStack.begin() + index);
+        index -= 1;
+    }
+
+    // Remove the parent layers that are not in the local stack of the defining layer.
+    for (size_t index = defPrimSpecIndex; index > 0;) {
+        index -= 1;
+        const SdfPrimSpecHandle primSpec = primStack[index];
+
+        // If the prim spec layer is a parent layer of the defining layer, then we keep
+        // it and add its children to the defining layers set.
+        SdfLayerHandle layer = primSpec->GetLayer();
+        if (hasSubLayerInSet(layer, definingLayers)) {
+            addSubLayers(layer, definingLayers);
+            continue;
+        }
+
+        // Otherwise, we remove the prim spec from the defining prim stack.
+        // We don't need to adjust the index since we are going backward.
+        primStack.erase(primStack.begin() + index);
+    }
+
+    return primStack;
+}
+
+SdfPrimSpecHandle getDefiningPrimSpec(const UsdPrim& prim)
+{
+    SdfPrimSpecHandleVector primSpecs = prim.GetPrimStack();
+    for (SdfPrimSpecHandle primSpec : primSpecs) {
+        if (!primSpec)
+            continue;
+
+        const SdfSpecifier spec = primSpec->GetSpecifier();
+        if (spec != SdfSpecifierDef)
+            continue;
+
+        return primSpec;
+    }
+
+    return {};
 }
 
 } // namespace MAYAUSD_NS_DEF
