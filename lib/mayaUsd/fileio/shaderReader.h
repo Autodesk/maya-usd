07cbd6ec33cfa1ccd90666d11724a4779e9d622b
Samuel Liu <samuel.liu@autodesk.com>
2023-06-20 14:10:21 -0400

    Remove Context supported related functions in Shader Reader

--- lib/mayaUsd/fileio/shaderReader.h
+++ lib/mayaUsd/fileio/shaderReader.h
@@ -41,26 +41,6 @@ public:
     MAYAUSD_CORE_PUBLIC
     UsdMayaShaderReader(const UsdMayaPrimReaderArgs&);
 
-    /*
-    /// The level of support a reader can offer for a given context
-    ///
-    /// A basic reader that gives correct results across most contexts should
-    /// report `Fallback`, while a specialized reader that really shines in a
-    /// given context should report `Supported` when the context is right and
-    /// `Unsupported` if the context is not as expected.
-    enum class ContextSupport
-    {
-        Supported,
-        Fallback,
-        Unsupported
-    };
-    */
-
-    /// This static function is expected for all shader readers and allows
-    /// declaring how well this class can support the current context:
-    MAYAUSD_CORE_PUBLIC
-    static ContextSupport CanImport(const UsdMayaJobImportArgs& importArgs);
-
     /// Get the Maya shading plug on \p mayaObject that corresponds to the USD
     /// attribute named \p usdAttrName.
     ///
