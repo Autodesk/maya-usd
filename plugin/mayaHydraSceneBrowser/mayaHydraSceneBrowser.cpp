69a93f25217fa6130de9bfe47cb1ff2d34b54e67
Béatrice Duval <beatrice.duval@autodesk.com>
2023-08-16 15:08:08 -0400

    HYDRA 480 - add title to browser, make it stay in front of Maya main window, add error message when browser cannot be shown

--- plugin/mayaHydraSceneBrowser/mayaHydraSceneBrowser.cpp
+++ plugin/mayaHydraSceneBrowser/mayaHydraSceneBrowser.cpp
@@ -8,6 +8,7 @@
 #include <maya/MFnPlugin.h>
 #include <maya/MArgList.h>
 #include <maya/MGlobal.h>
+#include <maya/MQtUtil.h>
 
 PXR_NAMESPACE_USING_DIRECTIVE
 
@@ -18,12 +19,15 @@ MStatus mayaHydraSceneBrowserCmd::doIt( const MArgList& args )
 {
     const SceneIndicesVector& sceneIndices = GetMayaHydraLibInterface().GetTerminalSceneIndices();
     if (sceneIndices.empty()) {
+        MGlobal::displayError("There are no registered terminal scene indices. The Hydra Scene Browser will not be shown.");
         return MS::kFailure;
     }
     else {
         if (!widget) {
-            widget = new HduiSceneIndexDebuggerWidget();
+            widget = new HduiSceneIndexDebuggerWidget(MQtUtil::mainWindow());
         }
+        widget->setWindowTitle("Hydra Scene Browser");
+        widget->setWindowFlags(Qt::Tool); // Ensure the browser stays in front of the main Maya window
         widget->SetSceneIndex("", sceneIndices.front(), true);
         widget->show();
     }
