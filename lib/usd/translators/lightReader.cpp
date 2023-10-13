110cc91e9ee3373fb0998b4ba1acb4789d3a9e26
Dan McGarry <dan.mcgarry@gmail.com>
2023-10-13 11:08:00 -0700

    Determine how to read UsdLux prims with an envvar
    This eliminates the need for the build var and allows this to be set at
    runtime

--- lib/usd/translators/lightReader.cpp
+++ lib/usd/translators/lightReader.cpp
@@ -15,27 +15,91 @@
 //
 #include <mayaUsd/fileio/primReaderRegistry.h>
 #include <mayaUsd/fileio/translators/translatorLight.h>
+#include <mayaUsd/fileio/translators/translatorRfMLight.h>
+
+#include <pxr/base/tf/envSetting.h>
 
 PXR_NAMESPACE_OPEN_SCOPE
 
-// Build variable used to import usd builtin
-// lights as maya lights
-#ifndef MAYA_USD_IMPORT_PXR_LIGHTS
+TF_DEFINE_ENV_SETTING(
+    MAYAUSD_IMPORT_RFM_LIGHTS,
+    false,
+    "Whether to import UsdLux as Renderman-for-Maya lights.");
+
+PXRUSDMAYA_DEFINE_READER(UsdLuxCylinderLight, args, context)
+{
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
+    return false;
+}
+
+PXRUSDMAYA_DEFINE_READER(UsdLuxDiskLight, args, context)
+{
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
+    return false;
+}
 
 PXRUSDMAYA_DEFINE_READER(UsdLuxDistantLight, args, context)
 {
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
     return UsdMayaTranslatorLight::Read(args, context);
+}
+
+PXRUSDMAYA_DEFINE_READER(UsdLuxDomeLight, args, context)
+{
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
+    return false;
+}
+
+PXRUSDMAYA_DEFINE_READER(UsdLuxGeometryLight, args, context)
+{
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
+    return false;
 }
 
 PXRUSDMAYA_DEFINE_READER(UsdLuxRectLight, args, context)
 {
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
     return UsdMayaTranslatorLight::Read(args, context);
 }
 
 PXRUSDMAYA_DEFINE_READER(UsdLuxSphereLight, args, context)
 {
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
     return UsdMayaTranslatorLight::Read(args, context);
 }
-#endif
+
+// Moving to use PXRUSDMAYA_DEFINE_READER_FOR_USD_TYPE in anticipation of
+// codeless schemas for UsdRi types to be available soon!
+PXRUSDMAYA_DEFINE_READER_FOR_USD_TYPE(PxrAovLight, args, context)
+{
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
+    return false;
+}
+
+// Moving to use PXRUSDMAYA_DEFINE_READER_FOR_USD_TYPE in anticipation of
+// codeless schemas for UsdRi types to be available soon!
+PXRUSDMAYA_DEFINE_READER_FOR_USD_TYPE(PxrEnvDayLight, args, context)
+{
+    if (TfGetEnvSetting(MAYAUSD_IMPORT_RFM_LIGHTS)) {
+        return UsdMayaTranslatorRfMLight::Read(args, context);
+    }
+    return false;
+}
 
 PXR_NAMESPACE_CLOSE_SCOPE
