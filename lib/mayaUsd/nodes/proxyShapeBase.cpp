0c28b99ab78ffa99d694c9f6d2df3481d2d292d3
Zhicheng Ye <Zhicheng.Ye@al.com.au>
2024-04-12 22:55:02 +1000

    EMSUSD-1091 Save and restore non local edit target layer

--- lib/mayaUsd/nodes/proxyShapeBase.cpp
+++ lib/mayaUsd/nodes/proxyShapeBase.cpp
@@ -1168,8 +1168,14 @@ MStatus MayaUsdProxyShapeBase::computeInStageDataCached(MDataBlock& dataBlock)
 
         copyLayerMutingFromAttribute(*this, layerNameMap, *finalUsdStage);
 
-        if (!_targetLayer)
-            _targetLayer = getTargetLayerFromAttribute(*this, *finalUsdStage);
+        UsdEditTarget editTarget;
+        if (!_targetLayer) {
+            editTarget = getEditTargetFromAttribute(*this, *finalUsdStage);
+            if (editTarget.IsValid()) {
+                _targetLayer = editTarget.GetLayer();
+            }
+        }
+
         updateShareMode(sharedUsdStage, unsharedUsdStage, loadSet);
         // Note: setting the target layer must be done after updateShareMode()
         //       because the session layer changes when switching between shared
@@ -1179,9 +1185,13 @@ MStatus MayaUsdProxyShapeBase::computeInStageDataCached(MDataBlock& dataBlock)
             // Note: it is possible the cached edit target layer is no longer valid,
             //       for example if it was deleted. Trying to set an invalid layer would
             //       throw an exception.
-            if (isLayerInStage(_targetLayer, *finalUsdStage)) {
-                finalUsdStage->SetEditTarget(_targetLayer);
+            if (isLayerInStage(_targetLayer, *finalUsdStage)
+                && _targetLayer != editTarget.GetLayer()) {
+                editTarget = UsdEditTarget(_targetLayer);
             }
+        }
+        if (editTarget.IsValid()) {
+            finalUsdStage->SetEditTarget(editTarget);
         }
     }
 
