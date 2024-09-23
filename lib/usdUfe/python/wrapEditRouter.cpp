85bcb08065a17cbf41311425cc7a52e2ddd7475b
Julien Frantz <frantzy@illum-mg.fr>
2024-09-23 10:20:27 +0200

    Python overload to construct PrimMetadataEditRouterContext omitting metadataKeyPath

--- lib/usdUfe/python/wrapEditRouter.cpp
+++ lib/usdUfe/python/wrapEditRouter.cpp
@@ -138,14 +138,6 @@ AttributeEditRouterContextInit(const PXR_NS::UsdPrim& prim, const PXR_NS::TfToke
     return new UsdUfe::AttributeEditRouterContext(prim, attributeName);
 }
 
-UsdUfe::PrimMetadataEditRouterContext* PrimMetadataEditRouterContextInit(
-    const PXR_NS::UsdPrim& prim,
-    const PXR_NS::TfToken& metadataName,
-    const PXR_NS::TfToken& metadataKeyPath = {})
-{
-    return new UsdUfe::PrimMetadataEditRouterContext(prim, metadataName, metadataKeyPath);
-}
-
 // Note: due to a limitation of boost::python, we cannot pass shared-pointer
 //       between Python and C++. See this stack overflow answer for an explanation:
 //       https://stackoverflow.com/questions/20825662/boost-python-argument-types-did-not-match-c-signature
@@ -209,5 +201,6 @@ void wrapEditRouter()
 
     using PrimMdThis = UsdUfe::PrimMetadataEditRouterContext;
     class_<PrimMdThis, boost::noncopyable>("PrimMetadataEditRouterContext", no_init)
-        .def("__init__", make_constructor(PrimMetadataEditRouterContextInit));
+        .def(init<const PXR_NS::UsdPrim&, const PXR_NS::TfToken&>())
+        .def(init<const PXR_NS::UsdPrim&, const PXR_NS::TfToken&, const PXR_NS::TfToken&>());
 }
