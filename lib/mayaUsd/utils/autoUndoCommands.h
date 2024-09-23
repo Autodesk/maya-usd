57b23cc92ab55e8fe4452d4f0a01aa951aaa2076
Pierre Baillargeon <Pierre.Baillargeon@Autodesk.com>
2024-09-23 12:00:07 -0400

    EMSUSD-949 up axis in USD export
    - Add "-upAxis" (-upa) option to the export command.
    - Document it.
    - Add upAxis to the export job arguments structure.
    - Add upAxis and its possible values to the export tokens.
    - Expose upAxis job args to Python.
    - Add up axis UI to the export dialog.
    - Add AutoUndoCommands class to execute and automatically undo a set of
      MEL commands.
    - Use AutoUndoCommand to automatically change and restore the up-axis.
    - Implement temporarily changing Maya up-axis. We rotate all root nodes
      90 degrees before and after the export.
    - We use undo so that the values are perfectly restored.
    - Add unit test.

---
+++ lib/mayaUsd/utils/autoUndoCommands.h
@@ -0,0 +1,67 @@
+//
+// Copyright 2024 Autodesk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef MAYAUSD_AUTOUNDOCOMMANDS_H
+#define MAYAUSD_AUTOUNDOCOMMANDS_H
+
+#include <mayaUsd/base/api.h>
+
+#include <string>
+
+namespace MAYAUSD_NS_DEF {
+
+/// Automatically undo a group of MEL script commands when destroyed or cleaned-up.
+///
+/// Sub-classes provide the undoable MEL script to execute and which will be undone.
+/// Users can call cleanup() to do the cleanup early.
+class MAYAUSD_CORE_PUBLIC AutoUndoCommands
+{
+public:
+    /// Execute the given commands in an undo block.
+    ///
+    /// If no commands are provided, we do nothing.
+    /// This allow sub-classes to cancel the execution if needed by providing
+    /// no commands.
+    AutoUndoCommands(const char* scriptName, const std::string& commands);
+
+    /// Undo the executed command if undo has not been disabled.
+    ~AutoUndoCommands();
+
+    /// Undo immediately if not already done and not disabled.
+    void undo();
+
+    /// Disable undo of the commands.
+    /// Can be used if undo is no longer deemed necessary.
+    void disableUndo();
+
+private:
+    /// Execute the given commands in an undo block.
+    ///
+    /// If no commands are provided, we do nothing.
+    /// This allow sub-classes to cancel the execution if needed by providing
+    /// no commands.
+    void _executeCommands(const std::string& commands);
+
+    /// Undo the executed command if undo has not been disabled.
+    void _undoCommands();
+
+    std::string _scriptName;
+    bool        _needUndo { false };
+};
+
+} // namespace MAYAUSD_NS_DEF
+
+#endif
