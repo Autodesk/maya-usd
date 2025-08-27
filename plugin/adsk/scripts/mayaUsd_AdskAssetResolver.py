77f2d2aa93c0f51f3743cac62accd44bd8baed13
Samuel Liu <samuel.liu@autodesk.com>
2025-08-27 15:47:08 -0400

    Save the changed mapping file on exit

--- plugin/adsk/scripts/mayaUsd_AdskAssetResolver.py
+++ plugin/adsk/scripts/mayaUsd_AdskAssetResolver.py
@@ -4,6 +4,35 @@ from maya.OpenMaya import MGlobal
 import AdskAssetResolver as ar
 from pxr import Ar as pxrAr
 import os
+import subprocess
+import platform
+from pathlib import Path
+
+def get_resolver_version():
+    ''' Get the version of the Autodesk Asset Resolver. '''
+    try:
+        adskResolver = pxrAr.GetUnderlyingResolver()
+        if adskResolver is not None:
+            version = ar.VersionInfo()
+            arVersion = version.split("\n")[0]
+            arVersion = arVersion.split(" ")[0]
+            print("Autodesk Asset Resolver Version:", arVersion)
+            return arVersion
+    except Exception as e:
+        # The Autodesk Asset Resolver is not available
+        print("Error getting Autodesk Asset Resolver version:", e)
+        return None
+
+def meets_min_version(targetVersion):
+    ''' Compare two version strings of the Resolver. '''
+    def version_tuple(v):
+        return tuple(map(int, (v.split("."))))
+
+    currentVersion = get_resolver_version()
+    if currentVersion is None:
+        return False
+
+    return (version_tuple(currentVersion) >= version_tuple(targetVersion))
 
 def include_maya_project_tokens():
     ''' Include Maya project tokens in the USD Asset Resolver. '''
@@ -21,12 +50,11 @@ def unload_maya_project_tokens():
     ar.AssetResolverContextDataManager.RemoveContextData("MayaUSDExtension")
 
 def load_mappingfile(mappingFilePath=None):
-    if mappingFilePath is not None:
-        os.environ["ADSK_AR_MAPPING_FILE"] = mappingFilePath
     try:
         adskResolver = pxrAr.GetUnderlyingResolver()
         ctx = adskResolver.GetCurrentContext()
         ctx = ar.AdskResolverContext()
+        ctx.SetMappingFile(mappingFilePath)
         adskResolver.RefreshContext(ctx)
     
     except Exception as e:
