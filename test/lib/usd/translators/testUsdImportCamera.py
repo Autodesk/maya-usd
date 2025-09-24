2852e21213d3097f374eb750c90fd2962723f339
Talis Barbalho <talis.barbalho@autodesk.com>
2025-09-24 13:19:32 -0400

    Uncomment tests

--- test/lib/usd/translators/testUsdImportCamera.py
+++ test/lib/usd/translators/testUsdImportCamera.py
@@ -156,13 +156,13 @@ class testUsdImportCamera(unittest.TestCase):
         }
         self._ValidateAnimValuesAtTimes(animCurveFn, expectedTimesToValues)
 
-        #animCurveFn = self._GetAnimCurveFnForPlugName(cameraFn, 'fStop')
-        #self.assertEqual(animCurveFn.numKeys(), 2)
-        #expectedTimesToValues = {
-        #    0.0: 1.7974684,
-        #    5.0: 7.7784810,
-        #}
-        #self._ValidateAnimValuesAtTimes(animCurveFn, expectedTimesToValues)
+        animCurveFn = self._GetAnimCurveFnForPlugName(cameraFn, 'fStop')
+        self.assertEqual(animCurveFn.numKeys(), 2)
+        expectedTimesToValues = {
+            0.0: 1.7974684,
+            5.0: 7.7784810,
+        }
+        self._ValidateAnimValuesAtTimes(animCurveFn, expectedTimesToValues)
 
     def testStageOpens(self):
         self.assertTrue(self._stage)
