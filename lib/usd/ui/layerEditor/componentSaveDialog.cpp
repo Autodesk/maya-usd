e5b085ba5a35e1ee8c0476a78d62c69602390fca
deboisj <julien.deboise@autodesk.com>
2025-12-04 14:18:30 -0500

    fix 2023 windows?

--- lib/usd/ui/layerEditor/componentSaveDialog.cpp
+++ lib/usd/ui/layerEditor/componentSaveDialog.cpp
@@ -14,6 +14,12 @@
 // limitations under the License.
 //
 
+// Avoid operator conflicts with QT vs USD.
+#ifndef QT_NO_FLOAT16_OPERATORS
+#define QT_NO_FLOAT16_OPERATORS
+#endif
+#include <bitset>
+
 #include "componentSaveDialog.h"
 
 #include "generatedIconButton.h"
