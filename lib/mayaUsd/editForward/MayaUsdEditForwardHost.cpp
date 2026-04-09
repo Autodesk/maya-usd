d739150cdc367943a5f1021d7070717936c45aca
ADS\deboisj <julien.deboise@autodesk.com>
2026-04-09 13:41:00 -0400

    clang

--- lib/mayaUsd/editForward/MayaUsdEditForwardHost.cpp
+++ lib/mayaUsd/editForward/MayaUsdEditForwardHost.cpp
@@ -38,8 +38,8 @@ bool forwardingOpenedUndoChunk = false;
 
 void MayaUsdEditForwardHost::ExecuteInCmd(std::function<void()> callback, bool immediate)
 {
-    // If requested to be run immediately, likely an explicit request to forward edits, 
-    // we can just create an undoable command and run it (no need to consider the UndoBlock 
+    // If requested to be run immediately, likely an explicit request to forward edits,
+    // we can just create an undoable command and run it (no need to consider the UndoBlock
     // context etc. below, that is only relevant when reacting to changes in the scene).
     if (immediate) {
         if (callback) {
