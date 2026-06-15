667ff82184d033bb7b2bcbdb28af984ee1cbe292
ADS\deboisj <julien.deboise@autodesk.com>
2026-06-15 06:39:17 -0400

    Fixed more linux compile issues.

--- lib/usdLayerEditor/lib/layerEditorCommands.cpp
+++ lib/usdLayerEditor/lib/layerEditorCommands.cpp
@@ -1026,7 +1026,7 @@ bool StitchLayersCmd::doIt(const SdfLayerHandle& /*layer*/)
 
         // Creates a set of the added subLayers, prevent duplicates.
         std::set<std::string> addedSublayerIds;
-        for (const auto& path : strongLayerSubLayers) {
+        for (const auto path : strongLayerSubLayers) {
             const auto existingLayer = SdfLayer::FindRelativeToLayer(strongestLayer, path);
             if (existingLayer) {
                 addedSublayerIds.insert(existingLayer->GetIdentifier());
